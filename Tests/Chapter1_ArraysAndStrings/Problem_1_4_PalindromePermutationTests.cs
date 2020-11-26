1135b3f6b42631b10a0032564a3f7fc12cf3314c
Alex Vallejo <vallejo.alex@gmail.com>
2020-11-26 09:00:32 -0800

    Add problems 1.3 and 1.4

---
+++ Tests/Chapter1_ArraysAndStrings/Problem_1_4_PalindromePermutationTests.cs
@@ -0,0 +1,27 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter1_ArraysAndStrings.Tests
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass()]
+    public class Problem_1_4_PalindromePermutationTests
+    {
+        [TestMethod()]
+        public void NullOrEmptyTest() => Problem_1_4_PalindromePermutation.Execute("").Should().BeFalse();
+
+        [TestMethod()]
+        public void CapsTest() => Problem_1_4_PalindromePermutation.Execute("Anna").Should().BeFalse();
+
+        [TestMethod()]
+        public void EvenCountsTest() => Problem_1_4_PalindromePermutation.Execute("anna").Should().BeTrue();
+
+        [TestMethod()]
+        public void OddCountsTest() => Problem_1_4_PalindromePermutation.Execute("anbna").Should().BeTrue();
+
+        [TestMethod()]
+        public void OutOfOrderTest() => Problem_1_4_PalindromePermutation.Execute("nnaba").Should().BeTrue();
+
+        [TestMethod()]
+        public void MissingPairsTest() => Problem_1_4_PalindromePermutation.Execute("tannab").Should().BeFalse();
+    }
+}
