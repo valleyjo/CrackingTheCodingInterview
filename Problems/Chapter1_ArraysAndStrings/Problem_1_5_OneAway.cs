3a45c6acac5f1670661cc0221de985533d62a12a
Alex Vallejo <vallejo.alex@gmail.com>
2020-11-27 20:06:22 -0800

    Add problem 1.5

---
+++ Problems/Chapter1_ArraysAndStrings/Problem_1_5_OneAway.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace CrackingTheCodingInterview.Problems.Chapter1_ArraysAndStrings
+{
+    public class Problem_1_5_OneAway
+    {
+        /*
+        edits: insert, remove, change
+        always:
+            - abs(len1 - len2) > 1 -> return false
+            - positions must be the same
+
+        insert:
+            - all the same letters, except one new one
+
+        remove:
+            - all the same letters except one new one
+
+        change:
+            - all the same letters except one is different
+
+        1) validity check
+        2) one iteration
+        3) maintain bool for within correctness window
+        4) when letter is different:
+            a. if still ok, mark as not OK. Move index fwd for longer str. If == len do nothing
+            b. if not OK, return false
+
+        afreddy
+              ^
+        freddy
+             ^
+        */
+        public static bool Execute(string one, string two)
+        {
+            if (string.IsNullOrEmpty(one) || string.IsNullOrEmpty(two))
+            {
+                return false;
+            }
+
+            // must always be one edit away
+            // difference > 1 means it's more than one insert / remove away
+            if (Math.Abs(one.Length - two.Length) > 1)
+            {
+                return false;
+            }
+
+            bool stillValid = true;
+            for (int oneIndex = 0, twoIndex = 0; oneIndex < one.Length && twoIndex < two.Length; oneIndex++, twoIndex++)
+            {
+                char oneChar = one[oneIndex];
+                char twoChar = two[twoIndex];
+
+                if (oneChar != twoChar)
+                {
+                    if (!stillValid)
+                    {
+                        return false;
+                    }
+
+                    stillValid = false;
+
+                    // normalize the indexes after a difference
+                    // removes and inserts are covered by moving the longer index forward an extra space
+                    // changes are covered automatically because the lengths should be the same and all next
+                    // chars must be equal
+                    if (one.Length > two.Length)
+                    {
+                        oneIndex++;
+                    }
+                    else if (two.Length > one.Length)
+                    {
+                        twoIndex++;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Tests/Chapter1_ArraysAndStrings/Problem_1_5_OneAwayTests.cs
@@ -0,0 +1,36 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter1_ArraysAndStrings.Tests
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass()]
+    public class Problem_1_5_OneAwayTests
+    {
+        [TestMethod()]
+        public void NullOrEmptyTest() => Problem_1_5_OneAway.Execute(null, string.Empty).Should().BeFalse();
+
+        [TestMethod()]
+        public void OneInsertAtEndTest() => Problem_1_5_OneAway.Execute("freddy", "freddya").Should().BeTrue();
+
+        [TestMethod()]
+        public void OneInsertAtStartTest() => Problem_1_5_OneAway.Execute("afreddy", "freddy").Should().BeTrue();
+
+        [TestMethod()]
+        public void OneInsertAtMiddleTest() => Problem_1_5_OneAway.Execute("freaddy", "freddy").Should().BeTrue();
+
+        [TestMethod()]
+        public void OneRemoveAtEndTest() => Problem_1_5_OneAway.Execute("freddy", "fredd").Should().BeTrue();
+
+        [TestMethod()]
+        public void OneRemoveAtStartTest() => Problem_1_5_OneAway.Execute("reddy", "freddy").Should().BeTrue();
+
+        [TestMethod()]
+        public void OneRemoveAtMiddleTest() => Problem_1_5_OneAway.Execute("fredy", "freddy").Should().BeTrue();
+
+        [TestMethod()]
+        public void ZeroEditsTest() => Problem_1_5_OneAway.Execute("Freddy", "Freddy").Should().BeTrue();
+
+        [TestMethod()]
+        public void TwoEditsAway() => Problem_1_5_OneAway.Execute("Freddy", "Fred").Should().BeFalse();
+    }
+}
