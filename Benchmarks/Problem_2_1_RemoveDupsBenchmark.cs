d0372bfaae44cee41b5975887abb35d46ae27d33
Alex Vallejo <vallejo.alex@gmail.com>
2020-12-08 11:17:52 -0800

    Add benchmark for problem 2.1

---
+++ Benchmarks/Problem_2_1_RemoveDupsBenchmark.cs
@@ -0,0 +1,35 @@
+﻿namespace CrackingTheCodingInterview.Benchmarks
+{
+    using System;
+    using BenchmarkDotNet.Attributes;
+    using CrackingTheCodingInterview.Problems.DataStructures;
+    using CrackingTheCodingInterview.Problems.Chapter2_LinkedLists;
+
+    [MemoryDiagnoser]
+    public class Problem_2_1_RemoveDupsBenchmark
+    {
+        private readonly AListNode<int> dataOne;
+        private readonly AListNode<int> dataTwo;
+
+        public Problem_2_1_RemoveDupsBenchmark()
+        {
+            var data = new int[10000];
+
+            var random = new Random();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = random.Next(100);
+            }
+
+            this.dataOne = AListNode<int>.FromArray(data);
+            this.dataTwo = AListNode<int>.FromArray(data);
+        }
+
+        [Benchmark]
+        public void WithSpaceTest() => Problem_2_1_RemoveDups.ExecuteWithSpace(this.dataOne);
+
+        [Benchmark]
+        public void WithoutSpaceTest() => Problem_2_1_RemoveDups.ExecuteWithoutSpace(this.dataTwo);
+    }
+}
