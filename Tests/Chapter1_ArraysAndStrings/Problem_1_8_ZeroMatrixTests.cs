744c9b67b1559278b22aa93b6c672639abfc0ace
Alex Vallejo <vallejo.alex@gmail.com>
2020-12-07 21:20:02 -0800

    Add problem 1.8

---
+++ Tests/Chapter1_ArraysAndStrings/Problem_1_8_ZeroMatrixTests.cs
@@ -0,0 +1,53 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter1_ArraysAndStrings.Tests
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Problem = Problem_1_8_ZeroMatrix;
+
+    [TestClass()]
+    public class Problem_1_8_ZeroMatrixTests
+    {
+        [TestMethod()]
+        public void OneZeroTest()
+        {
+            var matrix = new int[,]
+            {
+                { 1, 0, 1, 1, },
+                { 1, 1, 1, 1, },
+                { 1, 1, 1, 1, },
+            };
+
+            var zeroedMatrix = new int[,]
+            {
+                { 0, 0, 0, 0, },
+                { 1, 0, 1, 1, },
+                { 1, 0, 1, 1, },
+            };
+
+
+            Problem.Execute(matrix);
+            matrix.Should().BeEquivalentTo(zeroedMatrix);
+        }
+
+        [TestMethod()]
+        public void TwoZeroTest()
+        {
+            var matrix = new int[,]
+            {
+                { 1, 0, 1, 1, },
+                { 1, 1, 0, 1, },
+                { 1, 1, 1, 1, },
+            };
+
+            var zeroedMatrix = new int[,]
+            {
+                { 0, 0, 0, 0, },
+                { 0, 0, 0, 0, },
+                { 1, 0, 0, 1, },
+            };
+
+            Problem.Execute(matrix);
+            matrix.Should().BeEquivalentTo(zeroedMatrix);
+        }
+    }
+}
