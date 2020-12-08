70c508ec262a19d87e03470a8aed206b6cd8e076
Alex Vallejo <vallejo.alex@gmail.com>
2020-12-08 10:37:04 -0800

    Add problem 2.1

---
+++ Problems/Chapter2_LinkedLists/Problem_2_1_RemoveDups.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using CrackingTheCodingInterview.Problems.DataStructures;
+
+namespace CrackingTheCodingInterview.Problems.Chapter2_LinkedLists
+{
+    public static class Problem_2_1_RemoveDups
+    {
+        /*
+        1 -> 2 -> 3
+             ^    ^
+        */
+        public static void Execute(AListNode<int> head)
+        {
+            var hashSet = new HashSet<int>() { head.Data };
+            AListNode<int> prev = head;
+            AListNode<int> cur = prev.Next;
+
+            while (cur != null)
+            {
+                if (hashSet.Contains(cur.Data))
+                {
+                    prev.Next = cur.Next;
+                }
+                else
+                {
+                    prev = cur;
+                }
+
+                hashSet.Add(cur.Data);
+                cur = cur.Next;
+            }
+        }
+    }
+}
---
+++ Tests/Chapter2_LinkedLists/Problem_2_1_RemoveDupsTests.cs
@@ -0,0 +1,43 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter2_LinkedLists.Tests
+{
+    using CrackingTheCodingInterview.Problems.DataStructures;
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Problem = Problem_2_1_RemoveDups;
+
+    [TestClass()]
+    public class Problem_2_1_RemoveDupsTests
+    {
+        [TestMethod()]
+        public void DupInMiddleTest()
+        {
+            var original = AListNode<int>.FromArray(new int[] { 0, 1, 2, 3, 3, 4, });
+            Problem.Execute(original);
+            original.ToString().Should().Be("(0) -> (1) -> (2) -> (3) -> (4)");
+        }
+
+        [TestMethod()]
+        public void DupInEndTest()
+        {
+            var original = AListNode<int>.FromArray(new int[] { 0, 1, 2, 3, 3, });
+            Problem.Execute(original);
+            original.ToString().Should().Be("(0) -> (1) -> (2) -> (3)");
+        }
+
+        [TestMethod]
+        public void AllDupsTest()
+        {
+            var original = AListNode<int>.FromArray(new int[] { 1, 1, 1, 1, 1, });
+            Problem.Execute(original);
+            original.ToString().Should().Be("(1)");
+        }
+
+        [TestMethod]
+        public void DupAtStartTest()
+        {
+            var original = AListNode<int>.FromArray(new int[] { 0, 0, 1, });
+            Problem.Execute(original);
+            original.ToString().Should().Be("(0) -> (1)");
+        }
+    }
+}
