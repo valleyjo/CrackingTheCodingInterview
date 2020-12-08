65840c8d2ac276b0fc1c893edcf506c51a1fb5aa
Alex Vallejo <vallejo.alex@gmail.com>
2020-12-08 10:36:07 -0800

    Add problem 1.9

---
+++ Tests/Chapter1_ArraysAndStrings/Problem_1_9_StringRotationTests.cs
@@ -0,0 +1,20 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter1_ArraysAndStrings.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using CrackingTheCodingInterview.Problems.Chapter1_ArraysAndStrings;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using FluentAssertions;
+    using Problem = CrackingTheCodingInterview.Problems.Chapter1_ArraysAndStrings.Problem_1_9_StringRotation;
+
+    [TestClass()]
+    public class Problem_1_9_StringRotationTests
+    {
+        [TestMethod()]
+        public void IsRotationTest() => Problem.IsRotation("keyboard", "ardkeybo").Should().BeTrue();
+
+        [TestMethod()]
+        public void IsNotRotationTest() => Problem.IsRotation("keyboard", "keyboards").Should().BeFalse();
+    }
+}
