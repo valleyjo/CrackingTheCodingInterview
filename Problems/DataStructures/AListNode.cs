ad29f85a4ba2c6266bc7573eea311425bc769ebc
Alex Vallejo <vallejo.alex@gmail.com>
2020-12-08 10:36:36 -0800

    Add data structures

---
+++ Problems/DataStructures/AListNode.cs
@@ -0,0 +1,40 @@
+﻿namespace CrackingTheCodingInterview.Problems.DataStructures
+{
+    using System.Text;
+
+    public class AListNode<T>
+    {
+        public T Data { get; set; }
+        public AListNode<T> Next { get; set; }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            AListNode<T> node = this;
+            while (node.Next != null)
+            {
+                sb.Append($"({node.Data}) -> ");
+                node = node.Next;
+            }
+            sb.Append($"({node.Data})");
+
+            return sb.ToString();
+        }
+
+        public static AListNode<T> FromArray(T[] array)
+        {
+            var head = new AListNode<T>();
+            AListNode<T> cur = head;
+
+            foreach (T element in array)
+            {
+                var newNode = new AListNode<T>();
+                newNode.Data = element;
+                cur.Next = newNode;
+                cur = newNode;
+            }
+
+            return head.Next;
+        }
+    }
+}
---
+++ Tests/DataStructures/ALinkedListTests.cs
@@ -0,0 +1,58 @@
+﻿namespace CrackingTheCodingInterview.Tests.DataStructures
+{
+    using CrackingTheCodingInterview.Problems.DataStructures;
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass()]
+    public class ALinkedListTests
+    {
+        [TestMethod()]
+        public void ToStringSingleNodeTest()
+        {
+            var head = new AListNode<int>();
+            head.Data = 5;
+            head.ToString().Should().Be("(5)");
+        }
+
+        [TestMethod()]
+        public void ToStringManyNodeTest()
+        {
+            var head = new AListNode<int>();
+            head.Data = 5;
+
+            var nextNode = new AListNode<int>();
+            head.Next = nextNode;
+            nextNode.Data = 4;
+
+            var nextNextNode = new AListNode<int>();
+            nextNextNode.Data = 3;
+            nextNode.Next = nextNextNode;
+
+            head.ToString().Should().Be("(5) -> (4) -> (3)");
+        }
+
+        [TestMethod()]
+        public void FromArrayTest()
+        {
+            var actual = AListNode<int>.FromArray(new int[] { 0, 1, 2, 3, });
+
+            var expected = new AListNode<int>();
+            expected.Data = 0;
+
+            var nextNode = new AListNode<int>();
+            expected.Next = nextNode;
+            nextNode.Data = 1;
+
+            var nextNextNode = new AListNode<int>();
+            nextNextNode.Data = 2;
+            nextNode.Next = nextNextNode;
+
+            var nextNextNextNode = new AListNode<int>();
+            nextNextNextNode.Data = 3;
+            nextNextNode.Next = nextNextNextNode;
+
+            actual.ToString().Should().Be(expected.ToString());
+        }
+    }
+}
