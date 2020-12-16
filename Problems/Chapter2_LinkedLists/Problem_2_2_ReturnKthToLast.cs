d546185c9fc6d11eeb9736e2359c6557f280241a
Alex Vallejo <vallejo.alex@gmail.com>
2020-12-15 19:51:23 -0800

    Add chapter 2.2

--- Problems/Chapter2_LinkedLists/Problem_2_2_ReturnKthToLast.cs
+++ Problems/Chapter2_LinkedLists/Problem_2_2_ReturnKthToLast.cs
@@ -1,6 +1,55 @@
 ﻿namespace CrackingTheCodingInterview.Problems.Chapter2_LinkedLists
 {
-    public class Problem_2_2_ReturnKthToLast
+  using System;
+  using CrackingTheCodingInterview.Problems.DataStructures;
+
+  public class Problem_2_2_ReturnKthToLast
+  {
+    /*
+    count the list and then run through again stopping k from the end
+    O(N) two loops
+
+    once we are K from the begining, start the second runner. When first runner
+    reaches end, return second runner.
+
+    edge cases:
+    list length < k
+    null
+    k = 0
+    k = list length
+    */
+    public static int Execute(AListNode<int> head, int k)
     {
+      if (head == null || k < 0)
+      {
+        throw new ArgumentException();
+      }
+
+      AListNode<int> leader = head;
+
+      for (int i = 0; i < k; i++)
+      {
+        if (leader == null)
+        {
+          throw new ArgumentException($"k value '{k}' is less than length of list");
+        }
+
+        leader = leader.Next;
+      }
+
+      // set K at the begining of the list and continue
+      // at the end of the list, kBehind will be k behind the end
+      AListNode<int> kBehind = head;
+      while (leader != null)
+      {
+        leader = leader.Next;
+        if (kBehind.Next != null)
+        {
+          kBehind = kBehind.Next;
+        }
+      }
+
+      return kBehind.Data;
     }
+  }
 }
---
+++ Tests/Chapter2_LinkedLists/Problem_2_2_ReturnKthToLastTests.cs
@@ -0,0 +1,58 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter2_LinkedLists.Tests
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Text;
+  using CrackingTheCodingInterview.Problems.Chapter2_LinkedLists;
+  using CrackingTheCodingInterview.Problems.DataStructures;
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using Problem = Chapter2_LinkedLists.Problem_2_2_ReturnKthToLast;
+
+  [TestClass]
+  public class Problem_2_2_ReturnKthToLastTests
+  {
+    [TestMethod]
+    public void NullText()
+    {
+      Action act = () => Problem.Execute(null, -1);
+      act.Should().Throw<ArgumentException>();
+    }
+
+    [TestMethod]
+    public void NegativeParamTest()
+    {
+      Action act = () => Problem.Execute(new AListNode<int>(), -1);
+      act.Should().Throw<ArgumentException>();
+    }
+
+    [TestMethod]
+    public void KIsZeroTest()
+    {
+      var head = AListNode<int>.FromArray(new int[] { 0, 1, 2, 3, });
+      Problem.Execute(head, 0).Should().Be(3);
+    }
+
+    [TestMethod]
+    public void KIsLargerThanListLengthTest()
+    {
+      var head = AListNode<int>.FromArray(new int[] { 0, 1, 2, 3, 4, 5, 6, });
+      Action act = () => Problem.Execute(head, 10);
+      act.Should().Throw<ArgumentException>();
+    }
+
+    [TestMethod]
+    public void KthToLastTest()
+    {
+      var head = AListNode<int>.FromArray(new int[] { 0, 1, 2, 3, 4, 5, 6, });
+      Problem.Execute(head, 3).Should().Be(4);
+    }
+
+    [TestMethod]
+    public void KIsListLength()
+    {
+      var head = AListNode<int>.FromArray(new int[] { 0, 1, 2, 3, 4, 5, 6, });
+      Problem.Execute(head, 7).Should().Be(0);
+    }
+  }
+}
