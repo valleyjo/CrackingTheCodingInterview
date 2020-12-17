5264f501e79b968d0fe4abf4e288ddc9feb25622
Alex Vallejo <vallejo.alex@gmail.com>
2020-12-16 17:33:25 -0800

    Provide new ctor for AListNode

--- Problems/DataStructures/AListNode.cs
+++ Problems/DataStructures/AListNode.cs
@@ -1,42 +1,62 @@
 ﻿namespace CrackingTheCodingInterview.Problems.DataStructures
 {
-    using System.Text;
+  using System.Text;
 
-    public class AListNode<T>
+  /// <summary>
+  /// Basic linked list node. Contains Data and Next members. Prefixed with "A"
+  /// because Alex wrote this class.
+  /// </summary>
+  /// <typeparam name="T"></typeparam>
+  public class AListNode<T>
+  {
+    public AListNode()
+      : this(default)
     {
-        public T Data { get; set; }
-
-        public AListNode<T> Next { get; set; }
-
-        public static AListNode<T> FromArray(T[] array)
-        {
-            var head = new AListNode<T>();
-            AListNode<T> cur = head;
-
-            foreach (T element in array)
-            {
-                var newNode = new AListNode<T>();
-                newNode.Data = element;
-                cur.Next = newNode;
-                cur = newNode;
-            }
-
-            return head.Next;
-        }
-
-        public override string ToString()
-        {
-            var sb = new StringBuilder();
-            AListNode<T> node = this;
-            while (node.Next != null)
-            {
-                sb.Append($"({node.Data}) -> ");
-                node = node.Next;
-            }
-
-            sb.Append($"({node.Data})");
-
-            return sb.ToString();
-        }
     }
+
+    public AListNode(T data)
+      : this(data, null)
+    {
+    }
+
+    public AListNode(T data, AListNode<T> next)
+    {
+      this.Data = data;
+      this.Next = next;
+    }
+
+    public T Data { get; set; }
+
+    public AListNode<T> Next { get; set; }
+
+    public static AListNode<T> FromArray(T[] array)
+    {
+      var head = new AListNode<T>();
+      AListNode<T> cur = head;
+
+      foreach (T element in array)
+      {
+        var newNode = new AListNode<T>(element);
+        cur.Next = newNode;
+        cur = newNode;
+      }
+
+      return head.Next;
+    }
+
+    public override string ToString()
+    {
+      var sb = new StringBuilder();
+      AListNode<T> node = this;
+      while (node.Next != null)
+      {
+        sb.Append($"({node.Data}) -> ");
+        node = node.Next;
+      }
+
+      sb.Append($"({node.Data})");
+
+      return sb.ToString();
+    }
+  }
 }
--- Tests/DataStructures/ALinkedListTests.cs
+++ Tests/DataStructures/ALinkedListTests.cs
@@ -10,23 +10,19 @@
         [TestMethod]
         public void ToStringSingleNodeTest()
         {
-            var head = new AListNode<int>();
-            head.Data = 5;
+            var head = new AListNode<int>(5);
             head.ToString().Should().Be("(5)");
         }
 
         [TestMethod]
         public void ToStringManyNodeTest()
         {
-            var head = new AListNode<int>();
-            head.Data = 5;
+            var head = new AListNode<int>(5);
 
-            var nextNode = new AListNode<int>();
+            var nextNode = new AListNode<int>(4);
             head.Next = nextNode;
-            nextNode.Data = 4;
 
-            var nextNextNode = new AListNode<int>();
-            nextNextNode.Data = 3;
+            var nextNextNode = new AListNode<int>(3);
             nextNode.Next = nextNextNode;
 
             head.ToString().Should().Be("(5) -> (4) -> (3)");
@@ -37,19 +33,15 @@
         {
             var actual = AListNode<int>.FromArray(new int[] { 0, 1, 2, 3, });
 
-            var expected = new AListNode<int>();
-            expected.Data = 0;
+            var expected = new AListNode<int>(0);
 
-            var nextNode = new AListNode<int>();
+            var nextNode = new AListNode<int>(1);
             expected.Next = nextNode;
-            nextNode.Data = 1;
 
-            var nextNextNode = new AListNode<int>();
-            nextNextNode.Data = 2;
+            var nextNextNode = new AListNode<int>(2);
             nextNode.Next = nextNextNode;
 
-            var nextNextNextNode = new AListNode<int>();
-            nextNextNextNode.Data = 3;
+            var nextNextNextNode = new AListNode<int>(3);
             nextNextNode.Next = nextNextNextNode;
 
             actual.ToString().Should().Be(expected.ToString());
