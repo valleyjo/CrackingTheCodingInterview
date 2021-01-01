d2994e43d279ac75f5b81861bfd73d5d72726700
Alex Vallejo <vallejo.alex@gmail.com>
2020-12-31 19:59:03 -0500

    Add problem 2.3

---
+++ Problems/Chapter2_LinkedLists/Problem_2_3_DeleteMiddleNode.cs
@@ -0,0 +1,33 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter2_LinkedLists
+{
+  using CrackingTheCodingInterview.Problems.DataStructures;
+
+  public static class Problem_2_3_DeleteMiddleNode
+  {
+    /*
+    Remove "middle" node w/ only access to this node.
+    end nodes or null nodes just return
+
+    1: Copy data
+    copy data of next node to this node.
+    remove the next node from the list
+    O(1)
+
+    2: Remove reference
+    can we set the reference of (this) to (this).Next?
+    I don't think this is possible
+    */
+    public static void Execute(AListNode<int> node)
+    {
+      if (node == null || node.Next == null)
+      {
+        return;
+      }
+
+      // copy the data from the next node
+      // skip the next node to remove it
+      node.Data = node.Next.Data;
+      node.Next = node.Next.Next;
+    }
+  }
+}
---
+++ Tests/Chapter2_LinkedLists/Problem_2_3_DeleteMiddleNodeTests.cs
@@ -0,0 +1,44 @@
+﻿namespace CrackingTheCodingInterview.Problems.Tests.Chapter2_LinkedLists
+{
+  using System;
+  using CrackingTheCodingInterview.Problems.DataStructures;
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using Problem = CrackingTheCodingInterview.Problems.Chapter2_LinkedLists.Problem_2_3_DeleteMiddleNode;
+
+  [TestClass]
+  public class Problem_2_3_DeleteMiddleNodeTests
+  {
+    [TestMethod]
+    public void NullTest()
+    {
+      Action act = () => Problem.Execute(null);
+      act.Should().NotThrow();
+    }
+
+    [TestMethod]
+    public void EndTest()
+    {
+      var list = AListNode<int>.FromArray(new int[] { 0 });
+      Action act = () => Problem.Execute(list);
+      act.Should().NotThrow();
+    }
+
+    [TestMethod]
+    public void RemoveMiddleTest()
+    {
+      var list = AListNode<int>.FromArray(new int[] { 0, 1, 2, 3, 4, 5 });
+      var middle = list.Next;
+      Problem.Execute(middle);
+      list.ToString().Should().Be("(0) -> (2) -> (3) -> (4) -> (5)");
+    }
+
+    [TestMethod]
+    public void RemoveSecondToLastTest()
+    {
+      var list = AListNode<int>.FromArray(new int[] { 0, 1, });
+      Problem.Execute(list);
+      list.ToString().Should().Be("(1)");
+    }
+  }
+}
