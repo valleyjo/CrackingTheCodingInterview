c0b1e24ce941adb79e3521850244196b9bd790f6
Alex Vallejo <vallejo.alex@gmail.com>
2020-12-31 21:02:29 -0500

    Add problem 2.4

---
+++ Problems/Chapter2_LinkedLists/Problem_2_4_Partition.cs
@@ -0,0 +1,68 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter2_LinkedLists
+{
+  using System.Collections.Generic;
+  using CrackingTheCodingInterview.Problems.DataStructures;
+
+  public static class Problem_2_4_Partition
+  {
+    /*
+    Create two new lists: one < partition, two >= partition
+    go through the input list and copy the data to the right list.
+    append both lists for the return value.
+    O(N) time -> best possible
+    O(n) space -> can it be done in place?
+    maintain pointers for "lower" half and "upper half"
+    Pointers can be at the start and the end.
+    for each node:
+      remove it from the list
+      append it to the begining or end
+    return the start of the list
+     */
+    public static AListNode<int> Execute(AListNode<int> node, int partition)
+    {
+      if (node == null)
+      {
+        return null;
+      }
+
+      var lowerHalf = new List<int>();
+      var upperHalf = new List<int>();
+      AListNode<int> cur = node;
+      while (cur != null)
+      {
+        if (cur.Data < partition)
+        {
+          lowerHalf.Add(cur.Data);
+        }
+        else
+        {
+          upperHalf.Add(cur.Data);
+        }
+
+        cur = cur.Next;
+      }
+
+      BuildList(lowerHalf, out AListNode<int> lowerHalfHead, out AListNode<int> lowerHalfTail);
+      BuildList(upperHalf, out AListNode<int> upperHalfHead, out _);
+
+      lowerHalfTail.Next = upperHalfHead;
+      return lowerHalfHead;
+    }
+
+    private static void BuildList(List<int> lowerHalf, out AListNode<int> head, out AListNode<int> tail)
+    {
+      // create a dummy node to maintain the head
+      tail = new AListNode<int>();
+      head = tail;
+      foreach (int val in lowerHalf)
+      {
+        var next = new AListNode<int>(val);
+        tail.Next = next;
+        tail = next;
+      }
+
+      // head was a dummy node so remove it from the from of the list now
+      head = head.Next;
+    }
+  }
+}
---
+++ Tests/Chapter2_LinkedLists/Problem_2_4_PartitionTests.cs
@@ -0,0 +1,27 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter2_LinkedLists.Tests
+{
+  using System;
+  using CrackingTheCodingInterview.Problems.DataStructures;
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using Problem = Problems.Chapter2_LinkedLists.Problem_2_4_Partition;
+
+  [TestClass]
+  public class Problem_2_4_PartitionTests
+  {
+    [TestMethod]
+    public void NullTest()
+    {
+      Action act = () => Problem.Execute(null, 0);
+      act.Should().NotThrow();
+    }
+
+    [TestMethod]
+    public void PartitionTest()
+    {
+      var list = AListNode<int>.FromArray(new int[] { 5, 1, 2, });
+      var partitionedList = Problem.Execute(list, 5);
+      partitionedList.ToString().Should().Be("(1) -> (2) -> (5)");
+    }
+  }
+}
