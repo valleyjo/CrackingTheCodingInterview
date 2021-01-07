bda64e9b39d798bb5a3117f2f5ea4d5f012ec46e
Alex Vallejo <vallejo.alex@gmail.com>
2021-01-07 09:28:59 -0800

    Add problem 3.2

---
+++ Problems/Chapter3_StacksAndQueues/Problem_3_2_MinStack.cs
@@ -0,0 +1,64 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter3_StacksAndQueues
+{
+  using System;
+  using System.Collections.Generic;
+
+  public class Problem_3_2_MinStack
+  {
+    private readonly List<MinStackRecord> data;
+    private int minValue;
+
+    public Problem_3_2_MinStack()
+    {
+      this.data = new List<MinStackRecord>();
+      this.minValue = int.MaxValue;
+    }
+
+    public void Push(int value)
+    {
+      if (value < this.minValue)
+      {
+        this.minValue = value;
+      }
+
+      this.data.Add(new MinStackRecord(value, this.minValue));
+    }
+
+    public int Pop()
+    {
+      this.ThrowIfEmpty();
+      int value = this.data[^1].Data;
+      this.data.RemoveAt(this.data.Count - 1);
+      return value;
+    }
+
+    public bool IsEmpty() => this.data.Count > 0;
+
+    public int GetMin()
+    {
+      this.ThrowIfEmpty();
+      return this.data[^1].MinValue;
+    }
+
+    private void ThrowIfEmpty()
+    {
+      if (this.IsEmpty())
+      {
+        throw new InvalidOperationException("Stack is empty");
+      }
+    }
+
+    private struct MinStackRecord
+    {
+      public MinStackRecord(int value, int minValue)
+      {
+        this.MinValue = minValue;
+        this.Data = value;
+      }
+
+      public int Data { get; }
+
+      public int MinValue { get; }
+    }
+  }
+}
---
+++ Tests/Chapter3_StacksAndQueues/Problem_3_2_MinStackTests.cs
@@ -0,0 +1,33 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter3_StacksAndQueues.Tests
+{
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using MinStack = Problem_3_2_MinStack;
+
+  [TestClass]
+  public class Problem_3_2_MinStackTests
+  {
+    [TestMethod]
+    public void PushTest()
+    {
+      var stack = new MinStack();
+      stack.Push(4);
+      stack.Pop().Should().Be(4);
+    }
+
+    [TestMethod]
+    public void PopTest()
+    {
+    }
+
+    [TestMethod]
+    public void IsEmptyTest()
+    {
+    }
+
+    [TestMethod]
+    public void GetMinTest()
+    {
+    }
+  }
+}
