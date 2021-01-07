24da0bab0d3ab52673c2be0f74ac159638a2bd77
Alex Vallejo <vallejo.alex@gmail.com>
2021-01-06 20:53:19 -0800

    Add problem 3.1

---
+++ Problems/Chapter3_StacksAndQueues/Problem_3_1_ThreeInOne.cs
@@ -0,0 +1,108 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter3_StacksAndQueues
+{
+  using System;
+  using System.Text;
+
+  public class Problem_3_1_ThreeInOne
+  {
+    private const int NumberOfStacks = 3;
+    private const uint MaxStackSize = uint.MaxValue / NumberOfStacks;
+    private readonly int[] data;
+    private readonly int[] stackIndexes;
+    private readonly uint stackSize;
+
+    public Problem_3_1_ThreeInOne(uint stackSize)
+    {
+      if (stackSize > MaxStackSize)
+      {
+        throw new ArgumentException($"{nameof(stackSize)} must be smaller than {uint.MaxValue / NumberOfStacks}", nameof(stackSize));
+      }
+
+      this.data = new int[stackSize * 3];
+      this.stackIndexes = new int[NumberOfStacks];
+      this.stackSize = stackSize;
+      for (int i = 0; i < NumberOfStacks; i++)
+      {
+        this.stackIndexes[i] = -1;
+      }
+    }
+
+    public void Push(uint stackId, int value)
+    {
+      this.ThrowIfInvalid(stackId);
+      if (this.IsFull(stackId))
+      {
+        throw new OutOfMemoryException($"stackSize {MaxStackSize} exceeded");
+      }
+
+      this.stackIndexes[stackId]++;
+      this.data[this.stackIndexes[stackId]] = value;
+    }
+
+    public int Pop(uint stackId)
+    {
+      this.ThrowIfInvalid(stackId);
+      int value = this.data[this.stackIndexes[stackId]];
+      this.stackIndexes[stackId]--;
+      return value;
+    }
+
+    public bool IsFull(uint stackId)
+    {
+      this.ThrowIfInvalid(stackId);
+      return !this.IsEmpty(stackId) && this.stackIndexes[stackId] == this.stackSize - 1;
+    }
+
+    public bool IsEmpty()
+    {
+      bool isEmpty = true;
+      foreach (int index in this.stackIndexes)
+      {
+        isEmpty &= index == -1;
+      }
+
+      return isEmpty;
+    }
+
+    public bool IsEmpty(uint stackId)
+    {
+      this.ThrowIfInvalid(stackId);
+      return this.stackIndexes[stackId] == -1;
+    }
+
+    public int Peak(uint stackId)
+    {
+      this.ThrowIfInvalid(stackId);
+      return this.data[this.stackIndexes[stackId]];
+    }
+
+    public override string ToString()
+    {
+      // TODO: this breaks for a stack size > int.maxValue
+      var stackSegments = new ArraySegment<int>[NumberOfStacks];
+      for (int i = 0; i < NumberOfStacks; i++)
+      {
+        stackSegments[i] = new ArraySegment<int>(this.data, (int)this.stackSize * i, (int)this.stackSize);
+      }
+
+      var builder = new StringBuilder();
+      for (int i = 0; i < stackSegments.Length; i++)
+      {
+        ArraySegment<int> stackSegment = stackSegments[i];
+        builder.Append($"stack {i}: [");
+        builder.Append(string.Join(", ", stackSegment));
+        builder.Append($"] | {Environment.NewLine}");
+      }
+
+      return builder.ToString();
+    }
+
+    private void ThrowIfInvalid(uint stackId)
+    {
+      if (stackId >= this.stackIndexes.Length)
+      {
+        throw new ArgumentException($"{nameof(stackId)} must be between 0 and {this.stackIndexes.Length}", nameof(stackId));
+      }
+    }
+  }
+}
---
+++ Tests/Chapter3_StacksAndQueues/Problem_3_1_ThreeInOneTests.cs
@@ -0,0 +1,95 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter3_StacksAndQueues.Tests
+{
+  using System;
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using MultiStack = Problem_3_1_ThreeInOne;
+
+  [TestClass]
+  public class Problem_3_1_ThreeInOneTests
+  {
+    [TestMethod]
+    public void ExceptionTest()
+    {
+      Action act = () => new MultiStack((uint.MaxValue / 3) + 1);
+      act.Should().Throw<ArgumentException>();
+
+      var validStack = new MultiStack(3);
+
+      act = () => validStack.Push(4, 0);
+      act.Should().Throw<ArgumentException>();
+
+      act = () => validStack.Pop(4);
+      act.Should().Throw<ArgumentException>();
+
+      act = () => validStack.Peak(4);
+      act.Should().Throw<ArgumentException>();
+
+      act = () => validStack.IsEmpty(4);
+      act.Should().Throw<ArgumentException>();
+    }
+
+    [TestMethod]
+    public void PushTest()
+    {
+      var stack = new MultiStack(5);
+      stack.Push(0, -1);
+      stack.Pop(0).Should().Be(-1);
+    }
+
+    [TestMethod]
+    public void IsEmptyTest()
+    {
+      var stack = new MultiStack(5);
+      stack.IsEmpty().Should().BeTrue();
+      stack.Push(0, -1);
+      stack.IsEmpty().Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void IsEmptyStackTest()
+    {
+      var stack = new MultiStack(5);
+      stack.IsEmpty(0).Should().BeTrue();
+      stack.Push(0, -5);
+      stack.IsEmpty(0).Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void PeakTest()
+    {
+      var stack = new MultiStack(5);
+      stack.Push(2, -10);
+      stack.Peak(2).Should().Be(-10);
+      stack.IsEmpty(2).Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void OutOfMemoryTest()
+    {
+      Action act = () =>
+      {
+        var stack = new MultiStack(5);
+        for (int i = 0; i <= 5; i++)
+        {
+          stack.Push(2, i);
+        }
+      };
+
+      act.Should().Throw<OutOfMemoryException>();
+    }
+
+    [TestMethod]
+    public void IsFullTest()
+    {
+      var stack = new MultiStack(5);
+      stack.IsFull(1).Should().BeFalse();
+      for (int i = 0; i < 5; i++)
+      {
+        stack.Push(1, i);
+      }
+
+      stack.IsFull(1).Should().BeTrue();
+    }
+  }
+}
