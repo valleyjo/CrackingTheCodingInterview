88c83cadc4d4e26aff539b7d9962fc6ee8417623
Alex Vallejo <vallejo.alex@gmail.com>
2021-01-13 11:00:43 -0800

    Add problem 3.4

---
+++ Problems/Chapter3_StacksAndQueues/Problem_3_4_QueueViaStacks.cs
@@ -0,0 +1,59 @@
+﻿namespace CrackingTheCodingInterview.Tests.Chapter3_StacksAndQueues
+{
+  using System;
+  using System.Collections.Generic;
+
+  /*
+  convert FIFO to LIFO
+
+  can use one stack as storage, and move between stacks over for each operaton
+
+  save time by only reversing operations when actually needed
+  */
+  public class Problem_3_4_QueueViaStacks
+  {
+    private readonly Stack<int> primary;
+    private readonly Stack<int> secondary;
+
+    public Problem_3_4_QueueViaStacks()
+    {
+      this.primary = new Stack<int>();
+      this.secondary = new Stack<int>();
+    }
+
+    public void Enqueue(int value)
+    {
+      this.primary.Push(value);
+    }
+
+    public int Dequeue()
+    {
+      if (this.primary.Count == 0 && this.secondary.Count == 0)
+      {
+        throw new InvalidOperationException("Can't dequeue empty queue");
+      }
+
+      // reverse the stacks
+      int value;
+      if (this.secondary.Count > 0)
+      {
+        value = this.secondary.Pop();
+      }
+      else
+      {
+        ReverseStacks(this.primary, this.secondary);
+        value = this.secondary.Pop();
+      }
+
+      return value;
+    }
+
+    private static void ReverseStacks(Stack<int> source, Stack<int> dest)
+    {
+      while (source.Count > 0)
+      {
+        dest.Push(source.Pop());
+      }
+    }
+  }
+}
---
+++ Tests/Chapter3_StacksAndQueues/Problem_3_4_QueueViaStacksTests.cs
@@ -0,0 +1,56 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter3_StacksAndQueues.Tests
+{
+  using System;
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using MyQueue = CrackingTheCodingInterview.Tests.Chapter3_StacksAndQueues.Problem_3_4_QueueViaStacks;
+
+  [TestClass]
+  public class Problem_3_4_QueueViaStacksTests
+  {
+    [TestMethod]
+    public void EnqueueTest()
+    {
+      var queue = new MyQueue();
+      queue.Enqueue(5);
+    }
+
+    [TestMethod]
+    public void DequeueExceptionTest()
+    {
+      var queue = new MyQueue();
+      Action act = () => queue.Dequeue();
+      act.Should().Throw<InvalidOperationException>();
+    }
+
+    [TestMethod]
+    public void DequeueTest()
+    {
+      var queue = new MyQueue();
+      queue.Enqueue(5);
+      queue.Enqueue(15);
+      queue.Dequeue().Should().Be(5);
+      queue.Dequeue().Should().Be(15);
+    }
+
+    [TestMethod]
+    public void ManyOperationsTest()
+    {
+      var queue = new MyQueue();
+      queue.Enqueue(5);
+      queue.Enqueue(10);
+      queue.Enqueue(15);
+      queue.Enqueue(20);
+      queue.Dequeue().Should().Be(5);
+      queue.Enqueue(25);
+      queue.Enqueue(30);
+      queue.Enqueue(35);
+      queue.Dequeue().Should().Be(10);
+      queue.Dequeue().Should().Be(15);
+      queue.Dequeue().Should().Be(20);
+      queue.Dequeue().Should().Be(25);
+      queue.Dequeue().Should().Be(30);
+      queue.Dequeue().Should().Be(35);
+    }
+  }
+}
