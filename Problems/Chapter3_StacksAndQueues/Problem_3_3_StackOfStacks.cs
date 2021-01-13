d27a426c0ce39f5fe93a743a61705d49bca4d0c8
Alex Vallejo <vallejo.alex@gmail.com>
2021-01-13 10:59:56 -0800

    Add problem 3.3

---
+++ Problems/Chapter3_StacksAndQueues/Problem_3_3_StackOfStacks.cs
@@ -0,0 +1,71 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter3_StacksAndQueues
+{
+  using System;
+  using System.Collections.Generic;
+
+  public class Problem_3_3_StackOfStacks
+  {
+    private readonly List<Stack<int>> data;
+    private readonly int capacity;
+    private int activeStack;
+
+    public Problem_3_3_StackOfStacks(int capacity)
+    {
+      if (capacity < 1)
+      {
+        throw new ArgumentOutOfRangeException(nameof(capacity), "Capacity must be greater than 0");
+      }
+
+      this.capacity = capacity;
+      this.data = new List<Stack<int>>() { new Stack<int>(), };
+      this.activeStack = 0;
+    }
+
+    public void Push(int value)
+    {
+      if (this.data[this.activeStack].Count < this.capacity)
+      {
+        this.data[this.activeStack].Push(value);
+      }
+      else
+      {
+        var nextStack = new Stack<int>();
+        nextStack.Push(value);
+        this.data.Add(nextStack);
+        this.activeStack++;
+      }
+    }
+
+    public int Pop()
+    {
+      if (this.data[this.activeStack].Count > 0)
+      {
+        return this.data[this.activeStack].Pop();
+      }
+      else
+      {
+        // resize the list?
+        this.data.RemoveAt(this.activeStack);
+        this.activeStack--;
+        if (this.activeStack == -1)
+        {
+          throw new InvalidOperationException("Cannot pop empty stack");
+        }
+
+        return this.data[this.activeStack].Pop();
+      }
+    }
+
+    public int Peak() => this.data[this.activeStack].Peek();
+
+    public int PopAt(int index)
+    {
+      if (index < 0 || index > this.activeStack)
+      {
+        throw new ArgumentOutOfRangeException("Index must be greater than zero and less than the number of stacks");
+      }
+
+      return this.data[index].Pop();
+    }
+  }
+}
---
+++ Tests/Chapter3_StacksAndQueues/Problem_3_3_StackOfStacksTests.cs
@@ -0,0 +1,69 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter3_StacksAndQueues.Tests
+{
+  using System;
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using StackOfStacks = Problem_3_3_StackOfStacks;
+
+  [TestClass]
+  public class Problem_3_3_StackOfStacksTests
+  {
+    [TestMethod]
+    public void InvalidCtorTest()
+    {
+      Action act = () => new StackOfStacks(-1);
+      act.Should().Throw<ArgumentOutOfRangeException>();
+    }
+
+    [TestMethod]
+    public void PushTest()
+    {
+      var stack = new StackOfStacks(1);
+      stack.Push(15);
+      stack.Push(20);
+      stack.Peak().Should().Be(20);
+    }
+
+    [TestMethod]
+    public void PopTest()
+    {
+      var stack = new StackOfStacks(1);
+      stack.Push(15);
+      stack.Push(20);
+      stack.Pop().Should().Be(20);
+      stack.Pop().Should().Be(15);
+    }
+
+    [TestMethod]
+    public void PopEmptyTest()
+    {
+      var stack = new StackOfStacks(1);
+      stack.Push(15);
+      stack.Push(20);
+      stack.Pop().Should().Be(20);
+      stack.Pop().Should().Be(15);
+      Action act = () => stack.Pop();
+      act.Should().Throw<InvalidOperationException>();
+    }
+
+    [TestMethod]
+    public void PopAtTest()
+    {
+      var stack = new StackOfStacks(1);
+      stack.Push(15);
+      stack.Push(20);
+      stack.PopAt(0).Should().Be(15);
+      stack.Pop().Should().Be(20);
+    }
+
+    [TestMethod]
+    public void PopAtExceptionTest()
+    {
+      var stack = new StackOfStacks(1);
+      Action act = () => stack.PopAt(-1);
+      act.Should().Throw<ArgumentOutOfRangeException>();
+      act = () => stack.PopAt(1);
+      act.Should().Throw<ArgumentOutOfRangeException>();
+    }
+  }
+}
