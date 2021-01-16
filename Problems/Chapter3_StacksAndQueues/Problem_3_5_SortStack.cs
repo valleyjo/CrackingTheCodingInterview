4b79b629cd4b48fe25ce9c081d5fd891fc1ded77
Alex Vallejo <vallejo.alex@gmail.com>
2021-01-15 20:42:09 -0800

    Add problem 3.5

---
+++ Problems/Chapter3_StacksAndQueues/Problem_3_5_SortStack.cs
@@ -0,0 +1,47 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter3_StacksAndQueues
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Data.SqlTypes;
+
+  public class Problem_3_5_SortStack
+  {
+    /*
+    modify stack in place
+    */
+    public static void Execute(Stack<int> stack)
+    {
+      if (stack == null)
+      {
+        throw new ArgumentNullException(nameof(stack));
+      }
+
+      var tmp = new Stack<int>();
+      int numberOfSortedElements = 0;
+      while (numberOfSortedElements < stack.Count)
+      {
+        int max = stack.Pop();
+        while (stack.Count > numberOfSortedElements)
+        {
+          if (stack.Peek() > max)
+          {
+            tmp.Push(max);
+            max = stack.Pop();
+          }
+          else
+          {
+            tmp.Push(stack.Pop());
+          }
+        }
+
+        stack.Push(max);
+        numberOfSortedElements++;
+
+        while (tmp.Count > 0)
+        {
+          stack.Push(tmp.Pop());
+        }
+      }
+    }
+  }
+}
---
+++ Tests/Chapter3_StacksAndQueues/Problem_3_5_SortStackTests.cs
@@ -0,0 +1,56 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter3_StacksAndQueues.Tests
+{
+  using System;
+  using System.Collections.Generic;
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using Problem = CrackingTheCodingInterview.Problems.Chapter3_StacksAndQueues.Problem_3_5_SortStack;
+
+  [TestClass]
+  public class Problem_3_5_SortStackTests
+  {
+    [TestMethod]
+    public void BasicSortTest()
+    {
+      var stack = new Stack<int>();
+      stack.Push(1);
+      stack.Push(5);
+      stack.Push(-15);
+      stack.Push(3);
+      stack.Push(15);
+
+      Problem.Execute(stack);
+      stack.Pop().Should().Be(-15);
+      stack.Pop().Should().Be(1);
+      stack.Pop().Should().Be(3);
+      stack.Pop().Should().Be(5);
+      stack.Pop().Should().Be(15);
+    }
+
+    [TestMethod]
+    public void OneElementTest()
+    {
+      var expected = new Stack<int>();
+      expected.Push(1);
+      var stack = new Stack<int>();
+      stack.Push(1);
+      Problem.Execute(stack);
+      stack.Should().BeEquivalentTo(expected);
+    }
+
+    [TestMethod]
+    public void NullTest()
+    {
+      Action act = () => Problem.Execute(null);
+      act.Should().Throw<ArgumentNullException>();
+    }
+
+    [TestMethod]
+    public void EmptyTest()
+    {
+      var empty = new Stack<int>();
+      Problem.Execute(empty);
+      empty.Should().BeEquivalentTo(new Stack<int>());
+    }
+  }
+}
