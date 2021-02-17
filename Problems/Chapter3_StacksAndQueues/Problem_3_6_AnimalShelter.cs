90c8d42effba078f42a3d53e4b1e6c49fb5b6987
Alex Vallejo <vallejo.alex@gmail.com>
2021-02-17 09:30:08 -0800

    Add problem 3.6

---
+++ Problems/Chapter3_StacksAndQueues/Problem_3_6_AnimalShelter.cs
@@ -0,0 +1,98 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter3_StacksAndQueues
+{
+  using System;
+  using System.Collections.Generic;
+
+  /// <summary>
+  /// Use an enum for AnimalType but this could easily be a class
+  /// </summary>
+  public enum AnimalType
+  {
+    Cat = 0,
+    Dog = 1,
+  }
+
+  public class Problem_3_6_AnimalShelter
+  {
+    private readonly LinkedList<AnimalType> cats;
+    private readonly LinkedList<AnimalType> dogs;
+    private readonly LinkedList<AnimalType> oldestAnimal;
+
+    public Problem_3_6_AnimalShelter()
+    {
+      this.cats = new LinkedList<AnimalType>();
+      this.dogs = new LinkedList<AnimalType>();
+      this.oldestAnimal = new LinkedList<AnimalType>();
+    }
+
+    public void Enqueue(AnimalType incomingAnimal)
+    {
+      switch (incomingAnimal)
+      {
+        case AnimalType.Cat:
+          this.cats.AddLast(incomingAnimal);
+          break;
+        case AnimalType.Dog:
+          this.dogs.AddLast(incomingAnimal);
+          break;
+      }
+
+      // Add a copy into the oldest animal queue
+      this.oldestAnimal.AddLast(incomingAnimal);
+    }
+
+    public AnimalType DequeueOldest()
+    {
+      if (this.oldestAnimal.Count == 0)
+      {
+        throw new InvalidOperationException("No animals available");
+      }
+
+      AnimalType oldest = this.oldestAnimal.First.Value;
+      this.oldestAnimal.RemoveFirst();
+      switch (oldest)
+      {
+        case AnimalType.Cat:
+          this.cats.RemoveFirst();
+          break;
+        case AnimalType.Dog:
+        default:
+          this.dogs.RemoveFirst();
+          break;
+      }
+
+      return oldest;
+    }
+
+    public AnimalType DequeueDog() => this.GetOrThrow(AnimalType.Dog);
+
+    public AnimalType DequeueCat() => this.GetOrThrow(AnimalType.Cat);
+
+    private AnimalType GetOrThrow(AnimalType requestedAnimalType)
+    {
+      LinkedList<AnimalType> queue;
+
+      switch (requestedAnimalType)
+      {
+        case AnimalType.Cat:
+          queue = this.cats;
+          break;
+        case AnimalType.Dog:
+        default:
+          queue = this.dogs;
+          break;
+      }
+
+      if (queue.Count > 0)
+      {
+        AnimalType animal = queue.First.Value;
+        queue.RemoveFirst();
+        return animal;
+      }
+      else
+      {
+        throw new InvalidOperationException($"No animals available of type '{requestedAnimalType}'");
+      }
+    }
+  }
+}
---
+++ Tests/Chapter3_StacksAndQueues/Problem_3_6_AnimalShelterTests.cs
@@ -0,0 +1,61 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter3_StacksAndQueues.Tests
+{
+  using System;
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using AnimalShelter = CrackingTheCodingInterview.Problems.Chapter3_StacksAndQueues.Problem_3_6_AnimalShelter;
+
+  [TestClass]
+  public class Problem_3_6_AnimalShelterTests
+  {
+    [TestMethod]
+    public void DequeueOldestTest()
+    {
+      var shelter = new AnimalShelter();
+      shelter.Enqueue(AnimalType.Cat);
+      shelter.Enqueue(AnimalType.Dog);
+      shelter.DequeueOldest().Should().Be(AnimalType.Cat);
+      shelter.DequeueOldest().Should().Be(AnimalType.Dog);
+    }
+
+    [TestMethod]
+    public void DequeueEmptyTest()
+    {
+      var shelter = new AnimalShelter();
+      Action act = () => shelter.DequeueOldest();
+      act.Should().Throw<InvalidOperationException>().WithMessage("No animals available");
+    }
+
+    [TestMethod]
+    public void DequeueCatTest()
+    {
+      var shelter = new AnimalShelter();
+      shelter.Enqueue(AnimalType.Cat);
+      shelter.DequeueCat().Should().Be(AnimalType.Cat);
+    }
+
+    [TestMethod]
+    public void DequeueEmptyCatTest()
+    {
+      var shelter = new AnimalShelter();
+      Action act = () => shelter.DequeueCat();
+      act.Should().Throw<InvalidOperationException>().WithMessage("No animals available of type 'Cat'");
+    }
+
+    [TestMethod]
+    public void DequeueDogTest()
+    {
+      var shelter = new AnimalShelter();
+      shelter.Enqueue(AnimalType.Dog);
+      shelter.DequeueDog().Should().Be(AnimalType.Dog);
+    }
+
+    [TestMethod]
+    public void DequeueEmptyDogTest()
+    {
+      var shelter = new AnimalShelter();
+      Action act = () => shelter.DequeueDog();
+      act.Should().Throw<InvalidOperationException>().WithMessage("No animals available of type 'Dog'");
+    }
+  }
+}
