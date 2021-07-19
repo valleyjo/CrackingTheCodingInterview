c68944f2f1bc5b81d8464e887f73857af7f96e9b
Alex Vallejo <vallejo.alex@gmail.com>
2021-07-19 07:03:56 -0700

    Add binary tree node

---
+++ Problems/DataStructures/ABinaryTreeNode.cs
@@ -0,0 +1,20 @@
+﻿namespace CrackingTheCodingInterview.Problems.DataStructures
+{
+  public class ABinaryTreeNode
+  {
+    public ABinaryTreeNode(int value) => this.Value = value;
+
+    public ABinaryTreeNode(int value, ABinaryTreeNode left = null, ABinaryTreeNode right = null)
+    {
+      this.Value = value;
+      this.Left = left;
+      this.Right = right;
+    }
+
+    public ABinaryTreeNode Left { get; set; }
+
+    public ABinaryTreeNode Right { get; set; }
+
+    public int Value { get; set; }
+  }
+}
