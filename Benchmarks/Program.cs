663a2a04c90f82a05ca0f6fa46fb6a615b9d65c3
Alex Vallejo <vallejo.alex@gmail.com>
2021-07-19 07:04:33 -0700

    Add binary tree printer

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -1,12 +1,17 @@
 ﻿namespace CrackingTheCodingInterview.Benchmarks
 {
+  using System.Collections.Generic;
   using BenchmarkDotNet.Running;
+  using CrackingTheCodingInterview.Problems.DataStructures;
 
   public class Program
   {
     public static void Main()
     {
-      BenchmarkRunner.Run<Problem_2_1_RemoveDupsBenchmark>();
+      ////BenchmarkRunner.Run<Problem_2_1_RemoveDupsBenchmark>();
+      var nums = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, };
+      ABinaryTreeNode root = Problems.Chapter4_TreesAndGraphs.Problem_4_2_MinimalTree.Execute(nums);
+      ABinaryTreePrinter.Print(root);
     }
   }
 }
