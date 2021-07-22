f3a6a72d224cf040f224b0cd40009c80c39325db
Alex Vallejo <vallejo.alex@gmail.com>
2021-07-22 08:14:40 -0700

    Add tests for ABinaryTreeNode.GetHeight

---
+++ Tests/DataStructures/ABinaryTreeNodeTests.cs
@@ -0,0 +1,82 @@
+﻿namespace CrackingTheCodingInterview.Tests.DataStructures
+{
+  using CrackingTheCodingInterview.Problems.DataStructures;
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+  [TestClass]
+  public class ABinaryTreeNodeTests
+  {
+    [TestMethod]
+    public void GetHeightOneTest()
+    {
+      var node = new ABinaryTreeNode(0);
+      node.GetHeight().Should().Be(1);
+    }
+
+    [TestMethod]
+    public void GetHeightLeftTallerTest()
+    {
+      /*
+          1
+         / \
+        2   2
+       /     \
+      3       3
+       \
+        4
+       */
+      var node = new ABinaryTreeNode(1);
+      node.Left = new ABinaryTreeNode(2);
+      node.Right = new ABinaryTreeNode(2);
+      node.Right.Right = new ABinaryTreeNode(3);
+      node.Left.Left = new ABinaryTreeNode(3);
+      node.Left.Left.Right = new ABinaryTreeNode(4);
+
+      node.GetHeight().Should().Be(4);
+    }
+
+    [TestMethod]
+    public void GetHeightRightTallerTest()
+    {
+      /*
+          1
+         / \
+        2   2
+       /     \
+      3       3
+             /
+            4
+       */
+      var node = new ABinaryTreeNode(1);
+      node.Left = new ABinaryTreeNode(2);
+      node.Right = new ABinaryTreeNode(2);
+      node.Right.Right = new ABinaryTreeNode(3);
+      node.Left.Left = new ABinaryTreeNode(3);
+      node.Right.Right.Left = new ABinaryTreeNode(4);
+
+      node.GetHeight().Should().Be(4);
+    }
+
+    [TestMethod]
+    public void GetHeightBothSidesEqualTest()
+    {
+      /*
+          1
+         / \
+        2   2
+       /   / \
+      3   3   3
+       */
+      var node = new ABinaryTreeNode(1);
+      node.Left = new ABinaryTreeNode(2);
+      node.Right = new ABinaryTreeNode(2);
+      node.Right.Right = new ABinaryTreeNode(3);
+      node.Right.Left = new ABinaryTreeNode(3);
+      node.Left.Left = new ABinaryTreeNode(3);
+      node.Right.Right = new ABinaryTreeNode(3);
+
+      node.GetHeight().Should().Be(3);
+    }
+  }
+}
