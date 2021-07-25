f24b87d09d1d4830f0338308fbcf93385d7ce1ce
Alex Vallejo <vallejo.alex@gmail.com>
2021-07-25 12:41:05 -0700

    Add problem 4.4

---
+++ Problems/Chapter4_TreesAndGraphs/Problem_4_4_CheckBalanced.cs
@@ -0,0 +1,160 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter4_TreesAndGraphs
+{
+  using System;
+  using CrackingTheCodingInterview.Problems.DataStructures;
+
+  public static class Problem_4_4_CheckBalanced
+  {
+    private const int MinDifferenceAllowed = 1;
+
+    /// <summary>
+    /// Initial solution I came up with using the "GetHeight" function of the ABinaryTree class
+    /// </summary>
+    public static bool ExecuteNaive(ABinaryTreeNode root)
+    {
+      if (root == null)
+      {
+        return true;
+      }
+
+      return Math.Abs(GetHeight(root.Left, 0) - GetHeight(root.Right, 0)) <= MinDifferenceAllowed && ExecuteNaive(root.Left) && ExecuteNaive(root.Right);
+    }
+
+    public static bool ExecuteOptimizedStructReturn(ABinaryTreeNode root) => ExecuteOptimizedStructReturn(root, 0).Balanced;
+
+    public static bool ExecuteOptimizedClassReturn(ABinaryTreeNode root) => ExecuteOptimizedClassReturn(root, 0).Balanced;
+
+    public static bool ExecuteOptimizedIntReturn(ABinaryTreeNode root) => ExecuteOptimizedIntReturnInternal(root) != int.MinValue;
+
+    /// <summary>
+    /// Solution given in the book
+    /// </summary>
+    /// <returns>int.MinValue if tree is not balanced, height of tree otherwise</returns>
+    private static int ExecuteOptimizedIntReturnInternal(ABinaryTreeNode root)
+    {
+      if (root == null)
+      {
+        return 0;
+      }
+
+      int leftHeight = ExecuteOptimizedIntReturnInternal(root.Left);
+      if (leftHeight == int.MinValue)
+      {
+        return int.MinValue;
+      }
+
+      int rightHeigth = ExecuteOptimizedIntReturnInternal(root.Right);
+      if (rightHeigth == int.MinValue)
+      {
+        return int.MinValue;
+      }
+
+      if (Math.Abs(rightHeigth - leftHeight) > MinDifferenceAllowed)
+      {
+        return int.MinValue;
+      }
+
+      return Math.Max(rightHeigth, leftHeight) + 1;
+    }
+
+    /// <summary>
+    /// solution I came up with without hints or checking the book
+    /// </summary>
+    /// <param name="root"></param>
+    /// <param name="curDepth"></param>
+    /// <returns></returns>
+    private static ReturnValueStruct ExecuteOptimizedStructReturn(ABinaryTreeNode root, int curDepth)
+    {
+      if (root == null)
+      {
+        return new ReturnValueStruct(curDepth - 1, true);
+      }
+
+      ReturnValueStruct left = ExecuteOptimizedStructReturn(root.Left, curDepth + 1);
+
+      if (!left.Balanced)
+      {
+        return new ReturnValueStruct(left.Height, false);
+      }
+
+      ReturnValueStruct right = ExecuteOptimizedStructReturn(root.Right, curDepth + 1);
+      if (!right.Balanced)
+      {
+        return new ReturnValueStruct(right.Height, false);
+      }
+
+      bool balanced = Math.Abs(left.Height - right.Height) <= MinDifferenceAllowed;
+      int height = Math.Max(left.Height, right.Height);
+
+      return new ReturnValueStruct(height, balanced && left.Balanced && right.Balanced);
+    }
+
+    /// <summary>
+    /// solution I came up with without hints or checking the book
+    /// </summary>
+    /// <param name="root"></param>
+    /// <param name="curDepth"></param>
+    /// <returns></returns>
+    private static ReturnValueClass ExecuteOptimizedClassReturn(ABinaryTreeNode root, int curDepth)
+    {
+      if (root == null)
+      {
+        return new ReturnValueClass(curDepth - 1, true);
+      }
+
+      ReturnValueClass left = ExecuteOptimizedClassReturn(root.Left, curDepth + 1);
+
+      if (!left.Balanced)
+      {
+        return new ReturnValueClass(left.Height, false);
+      }
+
+      ReturnValueClass right = ExecuteOptimizedClassReturn(root.Right, curDepth + 1);
+      if (!right.Balanced)
+      {
+        return new ReturnValueClass(right.Height, false);
+      }
+
+      bool balanced = Math.Abs(left.Height - right.Height) <= MinDifferenceAllowed;
+      int height = Math.Max(left.Height, right.Height);
+
+      return new ReturnValueClass(height, balanced && left.Balanced && right.Balanced);
+    }
+
+    private static int GetHeight(ABinaryTreeNode root, int curDepth)
+    {
+      if (root == null)
+      {
+        return curDepth - 1;
+      }
+
+      return Math.Max(GetHeight(root.Left, curDepth + 1), GetHeight(root.Right, curDepth + 1));
+    }
+
+    private struct ReturnValueStruct
+    {
+      public ReturnValueStruct(int height, bool balanced)
+      {
+        this.Height = height;
+        this.Balanced = balanced;
+      }
+
+      public int Height { get; }
+
+      public bool Balanced { get; }
+    }
+
+    private class ReturnValueClass
+    {
+      public ReturnValueClass(int height, bool balanced)
+      {
+        this.Height = height;
+        this.Balanced = balanced;
+      }
+
+      public int Height { get; }
+
+      public bool Balanced { get;  }
+    }
+  }
+}
---
+++ Tests/Chapter4_TreesAndGraphs/Problem_4_4_CheckBalancedTests.cs
@@ -0,0 +1,87 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter4_TreesAndGraphs
+{
+  using System;
+  using CrackingTheCodingInterview.Problems.DataStructures;
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using Solution = CrackingTheCodingInterview.Problems.Chapter4_TreesAndGraphs.Problem_4_4_CheckBalanced;
+
+  [TestClass]
+  public class Problem_4_4_CheckBalancedTests
+  {
+    [TestMethod]
+    public void Naive_NullTest() => Solution.ExecuteNaive(null).Should().BeTrue();
+
+    [TestMethod]
+    public void Naive_FullTreeTest() => FullTreeTest(Solution.ExecuteNaive);
+
+    [TestMethod]
+    public void Naive_WithinBoundsTest() => WithinBoundsTest(Solution.ExecuteNaive);
+
+    [TestMethod]
+    public void Naive_OutOfBoundsTest() => OutOfBoundsTest(Solution.ExecuteNaive);
+
+    [TestMethod]
+    public void Struct_NullTest() => Solution.ExecuteOptimizedStructReturn(null).Should().BeTrue();
+
+    [TestMethod]
+    public void StructFaster_FullTreeTest() => FullTreeTest(Solution.ExecuteOptimizedStructReturn);
+
+    [TestMethod]
+    public void StructFaster_WithinBoundsTest() => WithinBoundsTest(Solution.ExecuteOptimizedStructReturn);
+
+    [TestMethod]
+    public void StructFaster_OutOfBoundsTest() => OutOfBoundsTest(Solution.ExecuteOptimizedStructReturn);
+
+    [TestMethod]
+    public void Class_NullTest() => Solution.ExecuteOptimizedClassReturn(null).Should().BeTrue();
+
+    [TestMethod]
+    public void Class_FullTreeTest() => FullTreeTest(Solution.ExecuteOptimizedClassReturn);
+
+    [TestMethod]
+    public void Class_WithinBoundsTest() => WithinBoundsTest(Solution.ExecuteOptimizedClassReturn);
+
+    [TestMethod]
+    public void Class_OutOfBoundsTest() => OutOfBoundsTest(Solution.ExecuteOptimizedClassReturn);
+
+    [TestMethod]
+    public void Int_NullTest() => Solution.ExecuteOptimizedIntReturn(null).Should().BeTrue();
+
+    [TestMethod]
+    public void Int_FullTreeTest() => FullTreeTest(Solution.ExecuteOptimizedIntReturn);
+
+    [TestMethod]
+    public void Int_WithinBoundsTest() => WithinBoundsTest(Solution.ExecuteOptimizedIntReturn);
+
+    [TestMethod]
+    public void Int_OutOfBoundsTest() => OutOfBoundsTest(Solution.ExecuteOptimizedIntReturn);
+
+    private static void FullTreeTest(Func<ABinaryTreeNode, bool> solution)
+    {
+      var tree = new ABinaryTreeNode(1);
+      tree.Left = new ABinaryTreeNode(2);
+      tree.Right = new ABinaryTreeNode(2);
+      solution(tree).Should().BeTrue();
+    }
+
+    private static void WithinBoundsTest(Func<ABinaryTreeNode, bool> solution)
+    {
+      var tree = new ABinaryTreeNode(1);
+      tree.Left = new ABinaryTreeNode(2);
+      tree.Left.Right = new ABinaryTreeNode(3);
+      tree.Right = new ABinaryTreeNode(2);
+      solution(tree).Should().BeTrue();
+    }
+
+    private static void OutOfBoundsTest(Func<ABinaryTreeNode, bool> solution)
+    {
+      var tree = new ABinaryTreeNode(1);
+      tree.Left = new ABinaryTreeNode(2);
+      tree.Left.Right = new ABinaryTreeNode(3);
+      tree.Left.Right.Left = new ABinaryTreeNode(4);
+      tree.Right = new ABinaryTreeNode(2);
+      solution(tree).Should().BeFalse();
+    }
+  }
+}
