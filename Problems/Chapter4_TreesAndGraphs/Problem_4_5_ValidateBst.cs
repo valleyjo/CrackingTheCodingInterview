8c4319e6bae3beee1e5a5f9a283174b7d845f17d
Alex Vallejo <vallejo.alex@gmail.com>
2021-07-25 12:42:18 -0700

    Add problem 4.5

---
+++ Problems/Chapter4_TreesAndGraphs/Problem_4_5_ValidateBst.cs
@@ -0,0 +1,25 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter4_TreesAndGraphs
+{
+  using System;
+  using CrackingTheCodingInterview.Problems.DataStructures;
+
+  public static class Problem_4_5_ValidateBst
+  {
+    public static bool Execute(ABinaryTreeNode root) => ExecuteInternal(root, int.MinValue, int.MaxValue);
+
+    private static bool ExecuteInternal(ABinaryTreeNode root, int min, int max)
+    {
+      if (root == null)
+      {
+        return true;
+      }
+
+      if (root.Value > max || root.Value < min)
+      {
+        return false;
+      }
+
+      return ExecuteInternal(root.Left, min, root.Value) && ExecuteInternal(root.Right, root.Value, max);
+    }
+  }
+}
---
+++ Tests/Chapter4_TreesAndGraphs/Problem_4_5_ValidateBstTests.cs
@@ -0,0 +1,81 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter4_TreesAndGraphs
+{
+  using System;
+  using CrackingTheCodingInterview.Problems.DataStructures;
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using Solution = CrackingTheCodingInterview.Problems.Chapter4_TreesAndGraphs.Problem_4_5_ValidateBst;
+
+  [TestClass]
+  public class Problem_4_5_ValidateBstTests
+  {
+    [TestMethod]
+    public void NullTest() => Solution.Execute(null).Should().BeTrue();
+
+    [TestMethod]
+    public void OneNodeTest() => Solution.Execute(new ABinaryTreeNode(1)).Should().BeTrue();
+
+    [TestMethod]
+    public void ValidDuplicatesTest()
+    {
+      var tree = new ABinaryTreeNode(0);
+      tree.Left = new ABinaryTreeNode(0);
+      tree.Left.Right = new ABinaryTreeNode(0);
+      tree.Left.Left = new ABinaryTreeNode(0);
+      tree.Right = new ABinaryTreeNode(10);
+      Solution.Execute(tree).Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void ValidSmallTest()
+    {
+      var tree = new ABinaryTreeNode(0);
+      tree.Left = new ABinaryTreeNode(-10);
+      tree.Right = new ABinaryTreeNode(10);
+      Solution.Execute(tree).Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void InvalidSmallTest()
+    {
+      var tree = new ABinaryTreeNode(0);
+      tree.Left = new ABinaryTreeNode(10);
+      tree.Right = new ABinaryTreeNode(-10);
+      Solution.Execute(tree).Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void InvalidOnLeftTest()
+    {
+      var tree = new ABinaryTreeNode(0);
+      tree.Left = new ABinaryTreeNode(-10);
+      tree.Left.Right = new ABinaryTreeNode(-5);
+      tree.Left.Right.Left = new ABinaryTreeNode(-15);
+      tree.Right = new ABinaryTreeNode(10);
+      Solution.Execute(tree).Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void InvalidOnRightTest()
+    {
+      var tree = new ABinaryTreeNode(0);
+      tree.Left = new ABinaryTreeNode(-10);
+      tree.Left.Right = new ABinaryTreeNode(-5);
+      tree.Left.Right.Left = new ABinaryTreeNode(-15);
+      tree.Right = new ABinaryTreeNode(10);
+      tree.Right.Left = new ABinaryTreeNode(15);
+      Solution.Execute(tree).Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void ValidTest()
+    {
+      var tree = new ABinaryTreeNode(0);
+      tree.Left = new ABinaryTreeNode(-10);
+      tree.Left.Right = new ABinaryTreeNode(-5);
+      tree.Left.Right.Left = new ABinaryTreeNode(-7);
+      tree.Right = new ABinaryTreeNode(10);
+      Solution.Execute(tree).Should().BeTrue();
+    }
+  }
+}
