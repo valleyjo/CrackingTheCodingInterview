cb7b5ba7ad6ace2b4e37adc543fad20098a1b1e6
Alex Vallejo <vallejo.alex@gmail.com>
2021-07-28 06:36:50 -0700

    Add problem 4.6

---
+++ Problems/Chapter4_TreesAndGraphs/Problem_4_6_Successor.cs
@@ -0,0 +1,36 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter4_TreesAndGraphs
+{
+  using CrackingTheCodingInterview.Problems.DataStructures;
+
+  public static class Problem_4_6_Successor
+  {
+    public static ABinaryTreeNode Execute(ABinaryTreeNode root)
+    {
+      if (root == null)
+      {
+        return null;
+      }
+
+      if (root.Right == null)
+      {
+        ABinaryTreeNode cur = root;
+        while (cur.Parent != null && ReferenceEquals(cur, cur.Parent.Right))
+        {
+          cur = cur.Parent;
+        }
+
+        return cur.Parent;
+      }
+      else
+      {
+        ABinaryTreeNode cur = root.Right;
+        while (cur.Left != null)
+        {
+          cur = cur.Left;
+        }
+
+        return cur;
+      }
+    }
+  }
+}
---
+++ Tests/Chapter4_TreesAndGraphs/Problem_4_6_SuccessorTests.cs
@@ -0,0 +1,46 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter4_TreesAndGraphs
+{
+  using CrackingTheCodingInterview.Problems.DataStructures;
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using Solution = CrackingTheCodingInterview.Problems.Chapter4_TreesAndGraphs.Problem_4_6_Successor;
+
+  [TestClass]
+  public class Problem_4_6_SuccessorTests
+  {
+    [TestMethod]
+    public void NullTest() => Solution.Execute(null).Should().BeNull();
+
+    [TestMethod]
+    public void OneNodeTest() => Solution.Execute(new ABinaryTreeNode(1)).Should().BeNull();
+
+    [TestMethod]
+    public void InRightSideTest()
+    {
+      var tree = new ABinaryTreeNode(0);
+      tree.Right = new ABinaryTreeNode(10);
+      tree.Right.Right = new ABinaryTreeNode(15);
+      tree.Right.Left = new ABinaryTreeNode(7);
+      Solution.Execute(tree).Should().BeSameAs(tree.Right.Left);
+    }
+
+    [TestMethod]
+    public void InLeftSideTest()
+    {
+      var tree = new ABinaryTreeNode(0);
+      tree.Left = new ABinaryTreeNode(-10);
+      tree.Left.Right = new ABinaryTreeNode(-10);
+      tree.Left.Left = new ABinaryTreeNode(-15);
+      Solution.Execute(tree).Should().BeNull();
+    }
+
+    [TestMethod]
+    public void AllNodesVisitedTest()
+    {
+      var tree = new ABinaryTreeNode(0);
+      tree.Right = new ABinaryTreeNode(10);
+      tree.Right.Right = new ABinaryTreeNode(20);
+      Solution.Execute(tree.Right.Right).Should().BeNull();
+    }
+  }
+}
