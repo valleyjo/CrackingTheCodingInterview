848b16eb31ef1af2e6873a0d5ecb446b9dd26ff5
Alex Vallejo <vallejo.alex@gmail.com>
2021-07-29 18:11:30 -0700

    Add problem 4.7

---
+++ Problems/Chapter4_TreesAndGraphs/Problem_4_7_BuildOrder.cs
@@ -0,0 +1,91 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter4_TreesAndGraphs
+{
+  using System;
+  using System.Collections.Generic;
+  using CrackingTheCodingInterview.Problems.DataStructures;
+
+  public static class Problem_4_7_BuildOrder
+  {
+    public static List<byte> Execute(List<int> projects, List<Dependency> dependencies)
+    {
+      if (projects == null || projects.Count == 0 || dependencies == null || dependencies.Count == 0)
+      {
+        throw new ArgumentException("projects and dependencies cannot be null or empty");
+      }
+
+      var result = new List<byte>(projects.Count);
+      BuildGraphs(projects, dependencies, out AGraph graph, out AGraph reverseGraph);
+      GenerateOrder(result, graph, reverseGraph);
+
+      return result;
+    }
+
+    private static void GenerateOrder(List<byte> result, AGraph graph, AGraph reverseGraph)
+    {
+      while (!graph.Empty)
+      {
+        var nodesToRemove = new List<byte>();
+        foreach (int node in graph)
+        {
+          // nodes with no outgoing connections should be built next
+          if (graph[node].Count == 0)
+          {
+            nodesToRemove.Add((byte)node);
+          }
+        }
+
+        if (nodesToRemove.Count == 0)
+        {
+          throw new ArgumentException("Circular dependency detected");
+        }
+
+        foreach (byte node in nodesToRemove)
+        {
+          // remove the edges for nodes that have been built
+          foreach (int outgoingConnection in reverseGraph[node])
+          {
+            graph.RemoveEdge(outgoingConnection, node);
+          }
+
+          reverseGraph.RemoveNode(node);
+          graph.RemoveNode(node);
+        }
+
+        result.AddRange(nodesToRemove);
+      }
+    }
+
+    // use two graphs to support O910 lookup and removal of a node
+    private static void BuildGraphs(List<int> projects, List<Dependency> dependencies, out AGraph graph, out AGraph reverseGraph)
+    {
+      graph = new AGraph(GraphType.Directional);
+      reverseGraph = new AGraph(GraphType.Directional);
+      foreach (Dependency item in dependencies)
+      {
+        graph.AddEdge(item.Source, item.Target);
+        reverseGraph.AddEdge(item.Target, item.Source);
+      }
+
+      // Add any projects without references to the graph, these can be built right away
+      foreach (int project in projects)
+      {
+        if (!graph.ContainsNode(project))
+        {
+          graph.AddNode(project);
+        }
+      }
+    }
+
+    public struct Dependency
+    {
+      public byte Source;
+      public byte Target;
+
+      public Dependency(byte source, byte target)
+      {
+        this.Source = source;
+        this.Target = target;
+      }
+    }
+  }
+}
---
+++ Tests/Chapter4_TreesAndGraphs/Problem_4_7_BuildOrderTests.cs
@@ -0,0 +1,73 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter4_TreesAndGraphs
+{
+  using System;
+  using System.Collections.Generic;
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using Solution = CrackingTheCodingInterview.Problems.Chapter4_TreesAndGraphs.Problem_4_7_BuildOrder;
+
+  /*
+  [TestClass]
+  public class Problem_4_7_BuildOrderTests
+  {
+    [TestMethod]
+    public void NullTest()
+    {
+      Action act = () => Solution.Execute(null, null);
+      act.Should().Throw<ArgumentException>();
+    }
+
+    [TestMethod]
+    public void EmptyTest()
+    {
+      Action act = () => Solution.Execute(new List<int>(), new List<Solution.Dependency>());
+      act.Should().Throw<ArgumentException>();
+    }
+
+    [TestMethod]
+    public void OneNodeTest()
+    {
+      var dependencies = new List<Solution.Dependency>()
+      {
+        new Solution.Dependency(1, 2),
+      };
+
+      var projects = new List<int>() { 1, 2, };
+
+      Solution.Execute(projects, dependencies).Should().BeEquivalentTo(new List<short> { 2, 1, });
+    }
+
+    [TestMethod]
+    public void CircularDependencyTest()
+    {
+      var dependencies = new List<Solution.Dependency>()
+      {
+        new Solution.Dependency(2, 1),
+        new Solution.Dependency(1, 2),
+      };
+
+      var projects = new List<int>() { 1, 2, };
+
+      Action act = () => Solution.Execute(projects, dependencies);
+      act.Should().Throw<ArgumentException>().WithMessage("Circular dependency detected");
+    }
+
+    [TestMethod]
+    public void BigGraphTest()
+    {
+      var dependencies = new List<Solution.Dependency>()
+      {
+        new Solution.Dependency(4, 1),
+        new Solution.Dependency(2, 6),
+        new Solution.Dependency(4, 2),
+        new Solution.Dependency(1, 6),
+        new Solution.Dependency(3, 4),
+      };
+
+      var projects = new List<int>() { 1, 2, 3, 4, 5, 6, };
+
+      Solution.Execute(projects, dependencies).Should().BeEquivalentTo(new List<int>() { 6, 5, 1, 2, 4, 3, });
+    }
+  }
+  */
+}
