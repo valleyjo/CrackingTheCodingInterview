531b47b1ad840d27818301c9087040575ababa4b
Alex Vallejo <vallejo.alex@gmail.com>
2021-08-21 16:14:40 -0700

    Add problem 4.10

---
+++ Problems/Chapter4_TreesAndGraphs/Problem_4_10_CheckSubtree.cs
@@ -0,0 +1,41 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter4_TreesAndGraphs
+{
+  using CrackingTheCodingInterview.Problems.DataStructures;
+
+  public static class Problem_4_10_CheckSubtree
+  {
+    public static bool Execute(ABinaryTreeNode tree, ABinaryTreeNode subtree)
+    {
+      // TODO: input validation
+      if (tree == null)
+      {
+        return false;
+      }
+
+      if (tree.Equals(subtree))
+      {
+        if (TreeDeepEquals(tree, subtree))
+        {
+          return true;
+        }
+      }
+
+      return Execute(tree.Left, subtree) || Execute(tree.Right, subtree);
+    }
+
+    private static bool TreeDeepEquals(ABinaryTreeNode tree, ABinaryTreeNode subtree)
+    {
+      if (tree == null && subtree == null)
+      {
+        return true;
+      }
+
+      if (tree != null && tree.Equals(subtree))
+      {
+        return TreeDeepEquals(tree.Left, subtree.Left) && TreeDeepEquals(tree.Right, subtree.Right);
+      }
+
+      return false;
+    }
+  }
+}
---
+++ Tests/Chapter4_TreesAndGraphs/Problem_4_10_checkSubtreeTests.cs
@@ -0,0 +1,65 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter4_TreesAndGraphs
+{
+  using CrackingTheCodingInterview.Problems.DataStructures;
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using Solution = CrackingTheCodingInterview.Problems.Chapter4_TreesAndGraphs.Problem_4_10_CheckSubtree;
+
+  [TestClass]
+  public class Problem_4_10_CheckSubtreeTests
+  {
+    [TestMethod]
+    public void NullTreeTest() => Solution.Execute(null, new ABinaryTreeNode(1)).Should().BeFalse();
+
+    [TestMethod]
+    public void NullSubTreeTest() => Solution.Execute(new ABinaryTreeNode(1), null).Should().BeFalse();
+
+    [TestMethod]
+    public void BothNullTest() => Solution.Execute(null, null).Should().BeFalse();
+
+    [TestMethod]
+    public void SingleNodeTest() => Solution.Execute(new ABinaryTreeNode(1), new ABinaryTreeNode(1)).Should().BeTrue();
+
+    [TestMethod]
+    public void Small_MatchesOnLeft()
+    {
+      var tree = new ABinaryTreeNode(1);
+      tree.Left = new ABinaryTreeNode(2);
+      var subtree = new ABinaryTreeNode(2);
+
+      Solution.Execute(tree, subtree).Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void Small_MatchesOnRight()
+    {
+      var tree = new ABinaryTreeNode(1);
+      tree.Right = new ABinaryTreeNode(2);
+      var subtree = new ABinaryTreeNode(2);
+
+      Solution.Execute(tree, subtree).Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void TwoPartialMatchesTest()
+    {
+      var tree = new ABinaryTreeNode(1);
+
+      tree.Right = new ABinaryTreeNode(
+        value: 2,
+        new ABinaryTreeNode(20),
+        new ABinaryTreeNode(7));
+
+      tree.Left = new ABinaryTreeNode(
+        value: 2,
+        right: new ABinaryTreeNode(7));
+
+      var subtree = new ABinaryTreeNode(
+        value: 2,
+        new ABinaryTreeNode(20),
+        new ABinaryTreeNode(7));
+
+      Solution.Execute(tree, subtree).Should().BeTrue();
+    }
+  }
+}
