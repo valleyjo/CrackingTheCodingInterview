0cfe8704350ff5ebf5dc5bf0e678d0660ae79ba2
Alex Vallejo <vallejo.alex@gmail.com>
2021-09-25 12:26:43 -0700

    Add problem 8.11

---
+++ Problems/Chapter8_RecursionAndDynamicProgramming/Problem_8_11_Coins.cs
@@ -0,0 +1,57 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter8_RecursionAndDynamicProgramming
+{
+  public static class Problem_8_11_Coins
+  {
+    public static int Execute(int amount)
+    {
+      if (amount <= 0)
+      {
+        return 0;
+      }
+
+      int[] cache = new int[amount + 1];
+      for (int i = 0; i <= amount; i++)
+      {
+        cache[i] = -1;
+      }
+
+      return Count(amount, cache);
+    }
+
+    private static int Count(int amount)
+    {
+      if (amount < 0)
+      {
+        return 0;
+      }
+
+      if (amount == 0)
+      {
+        return 1;
+      }
+
+      return Count(amount - 25) + Count(amount - 10) + Count(amount - 5) + Count(amount - 1);
+    }
+
+    private static int Count(int amount, int[] cache)
+    {
+      if (amount < 0)
+      {
+        return 0;
+      }
+
+      if (amount == 0)
+      {
+        return 1;
+      }
+
+      if (cache[amount] != -1)
+      {
+        return cache[amount];
+      }
+
+      cache[amount] = Count(amount - 25, cache) + Count(amount - 10, cache) + Count(amount - 5, cache) + Count(amount - 1, cache);
+      return cache[amount];
+    }
+  }
+}
---
+++ Tests/Chapter8_RecursionAndDynamicProgramming/Problem_8_11_CoinsTests.cs
@@ -0,0 +1,29 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter8_RecursionAndDynamicProgramming.Tests
+{
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using Solution = CrackingTheCodingInterview.Problems.Chapter8_RecursionAndDynamicProgramming.Problem_8_11_Coins;
+
+  [TestClass]
+  public class Problem_8_11_CoinsTests
+  {
+    [TestMethod]
+    public void ZeroTest() => Solution.Execute(0).Should().Be(0);
+
+    [TestMethod]
+    public void OneTest() => Solution.Execute(1).Should().Be(1);
+
+    [TestMethod]
+    public void TwoTest() => Solution.Execute(2).Should().Be(1);
+
+    [TestMethod]
+    public void FourTest() => Solution.Execute(4).Should().Be(1);
+
+    [TestMethod]
+    public void FiveTest() => Solution.Execute(5).Should().Be(2);
+
+    [TestMethod]
+    public void TwentyFiveTest() => Solution.Execute(5).Should().Be(2);
+  }
+  }
+}
