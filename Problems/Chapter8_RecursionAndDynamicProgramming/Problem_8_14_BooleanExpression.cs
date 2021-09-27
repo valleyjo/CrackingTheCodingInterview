fedb7aac7411b4b4b4898b584cd5298ac3d656a1
Alex Vallejo <vallejo.alex@gmail.com>
2021-09-27 11:02:52 -0700

    Add problem 8.14

---
+++ Problems/Chapter8_RecursionAndDynamicProgramming/Problem_8_14_BooleanExpression.cs
@@ -0,0 +1,53 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter8_RecursionAndDynamicProgramming
+{
+  using System;
+
+  public static class Problem_8_14_BooleanExpression
+  {
+    public static int Execute(string input, bool targetResult)
+    {
+      if (string.IsNullOrEmpty(input))
+      {
+        throw new ArgumentException("input cannot be null or empty", nameof(input));
+      }
+
+      if (input.Length == 1)
+      {
+        bool result = input == "1";
+        return result == targetResult ? 1 : 0;
+      }
+
+      int totalWaysMatchingTargetCount = 0;
+      for (int i = 1; i < input.Length; i += 2)
+      {
+        string left = input.Substring(0, i);
+        string right = input.Substring(i + 1);
+        char operation = input[i];
+
+        int leftTrueCount = Execute(left, true);
+        int leftFalseCount = Execute(left, false);
+        int rightTrueCount = Execute(right, true);
+        int rightFalseCount = Execute(right, false);
+        int totalWaysCount = (leftTrueCount + leftFalseCount) * (rightTrueCount + rightFalseCount);
+
+        int totalTrue = 0;
+        if (operation == '^')
+        {
+          totalTrue = (leftTrueCount * rightFalseCount) + (leftFalseCount * rightTrueCount);
+        }
+        else if (operation == '&')
+        {
+          totalTrue = leftTrueCount * rightTrueCount;
+        }
+        else if (operation == '|')
+        {
+          totalTrue = (leftTrueCount * rightTrueCount) + (leftFalseCount * rightTrueCount) + (leftTrueCount * rightFalseCount);
+        }
+
+        totalWaysMatchingTargetCount += targetResult ? totalTrue : totalWaysCount - totalTrue;
+      }
+
+      return totalWaysMatchingTargetCount;
+    }
+  }
+}
---
+++ Tests/Chapter8_RecursionAndDynamicProgramming/Problem_8_14_BooleanExpressionTests.cs
@@ -0,0 +1,61 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter8_RecursionAndDynamicProgramming.Tests
+{
+  using System;
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using Solution = CrackingTheCodingInterview.Problems.Chapter8_RecursionAndDynamicProgramming.Problem_8_14_BooleanExpression;
+
+  [TestClass]
+  public class Problem_8_14_BooleanExpressionTests
+  {
+    [TestMethod]
+    public void NullTest()
+    {
+      Action a = () => Solution.Execute(null, false);
+      a.Should().Throw<ArgumentException>().WithMessage("*input*");
+    }
+
+    [TestMethod]
+    public void EmptyTest()
+    {
+      Action a = () => Solution.Execute(string.Empty, false);
+      a.Should().Throw<ArgumentException>().WithMessage("*input*");
+    }
+
+    [TestMethod]
+    public void BaseCaseOneTest()
+    {
+      Solution.Execute("1", false).Should().Be(0);
+      Solution.Execute("1", true).Should().Be(1);
+    }
+
+    [TestMethod]
+    public void BaseCaseTwoTest()
+    {
+      Solution.Execute("0", false).Should().Be(1);
+      Solution.Execute("0", true).Should().Be(0);
+    }
+
+    [TestMethod]
+    public void OneExpressionTest()
+    {
+      Solution.Execute("0|1", true).Should().Be(1);
+      Solution.Execute("0^1", true).Should().Be(1);
+      Solution.Execute("0&1", true).Should().Be(0);
+    }
+
+    [TestMethod]
+    public void ComplexExpressionTest()
+    {
+      Solution.Execute("1^0|0|1", false).Should().Be(2);
+      Solution.Execute("0&0&0&1^1|0", true).Should().Be(10);
+    }
+
+    [TestMethod]
+    public void MemoVersionTest()
+    {
+      // TODO: implement memo version of problem 8.14
+      false.Should().BeTrue();
+    }
+  }
+}
