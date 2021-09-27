a8048d6a7b64b6fdfc0d74bd3b469b200c8b61d9
Alex Vallejo <vallejo.alex@gmail.com>
2021-09-27 11:02:18 -0700

    Add problem 8.13

---
+++ Problems/Chapter8_RecursionAndDynamicProgramming/Problem_8_13_StackOfBoxes.cs
@@ -0,0 +1,60 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter8_RecursionAndDynamicProgramming
+{
+  using System;
+  using System.Collections.Generic;
+
+  // use value type for easy copying
+  [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1649:File name should match first type name", Justification = "self contained class")]
+  public struct Box
+  {
+    public Box(uint width, uint height, uint depth)
+    {
+      this.Height = height;
+      this.Width = width;
+      this.Depth = depth;
+    }
+
+    public uint Height { get; private set; }
+
+    public uint Width { get; private set; }
+
+    public uint Depth { get; private set; }
+
+    // for easier debugging
+    public override string ToString() => $"[{this.Height}, {this.Width}, {this.Depth}]";
+
+    public bool IsSmallerThan(Box other) =>
+      this.Height < other.Height && this.Width < other.Width && this.Depth < other.Depth;
+  }
+
+  public static class Problem_8_13_StackOfBoxes
+  {
+    public static uint Execute(List<Box> boxes)
+    {
+      if (boxes == null)
+      {
+        throw new ArgumentNullException(nameof(boxes));
+      }
+
+      // pass in an "infinitly large" box to start so that the first box passes the IsSmallerThan conditional
+      return Execute(boxes, new HashSet<int>(boxes.Count), 0, new Box(uint.MaxValue, uint.MaxValue, uint.MaxValue));
+    }
+
+    private static uint Execute(List<Box> boxes, HashSet<int> usedBoxIndexes, uint currentHeight, Box lastBox)
+    {
+      uint tallestHeightSoFar = currentHeight;
+      for (int i = 0; i < boxes.Count; i++)
+      {
+        if (!usedBoxIndexes.Contains(i) && boxes[i].IsSmallerThan(lastBox))
+        {
+          usedBoxIndexes.Add(i);
+          uint heightForStackStartingWithThisBox = Execute(boxes, usedBoxIndexes, currentHeight + boxes[i].Height, boxes[i]);
+          tallestHeightSoFar = Math.Max(tallestHeightSoFar, heightForStackStartingWithThisBox);
+          usedBoxIndexes.Remove(i);
+        }
+      }
+
+      return tallestHeightSoFar;
+    }
+  }
+}
---
+++ Tests/Chapter8_RecursionAndDynamicProgramming/Problem_8_13_StackOfBoxesTests.cs
@@ -0,0 +1,69 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter8_RecursionAndDynamicProgramming.Tests
+{
+  using System;
+  using System.Collections.Generic;
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using Solution = CrackingTheCodingInterview.Problems.Chapter8_RecursionAndDynamicProgramming.Problem_8_13_StackOfBoxes;
+
+  // TODO: standalone tests for Box.IsSmallerThan(Box other)
+  [TestClass]
+  public class Problem_8_13_StackOfBoxesTests
+  {
+    [TestMethod]
+    public void NullTest()
+    {
+      Action a = () => Solution.Execute(null);
+      a.Should().Throw<ArgumentNullException>().WithMessage("*boxes*");
+    }
+
+    [TestMethod]
+    public void EmptyTest() => Solution.Execute(new List<Box>()).Should().Be(0);
+
+    [TestMethod]
+    public void OneBoxTest()
+    {
+      var boxes = new List<Box>()
+      {
+        new Box(1, 1, 1),
+      };
+
+      Solution.Execute(boxes).Should().Be(1);
+    }
+
+    [TestMethod]
+    public void NoStackingTest()
+    {
+      var boxes = new List<Box>()
+      {
+        new Box(2, 2, 1),
+        new Box(2, 1, 2),
+        new Box(5, 7, 1),
+      };
+
+      Solution.Execute(boxes).Should().Be(7);
+    }
+
+    [TestMethod]
+    public void StackingTest()
+    {
+      var boxes = new List<Box>()
+      {
+        new Box(1, 1, 1),  // [X]
+        new Box(2, 3, 2),  // [X]
+        new Box(4, 7, 4),  // [ ]
+        new Box(3, 8, 4),  // [X]
+        new Box(4, 10, 6), // [X]
+      };
+
+      Solution.Execute(boxes).Should().Be(22);
+    }
+
+    [TestMethod]
+    public void MemoVersionTest()
+    {
+      // TODO: implement memo version of problem 8.13
+      false.Should().BeTrue();
+    }
+  }
+}
