dd97f7595dec4329fc23d6d4309a817b7fd7ed1c
Alex Vallejo <vallejo.alex@gmail.com>
2021-09-27 21:02:07 -0700

    Rename folders

--- sln.cmd
+++ sln.cmd
@@ -1,2 +1,4 @@
+@echo OFF
 start CrackingTheCodingInterview.sln
 start https://docs.google.com/spreadsheets/d/1dMXyJKIv5hQHWGoFC7T5ioiK5N_RHbdPRzpZw8azqX0/edit?usp=sharing
+start %H%\OneDrive\Documents
