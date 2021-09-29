f52b0fe6cd526541da6993d84605691a927d5a6b
Alex Vallejo <vallejo.alex@gmail.com>
2021-09-29 11:40:17 -0700

    Add problem 10.3

---
+++ Problems/Chapter10_SortingAndSearching/Problem_10_03_SearchInRotatedArray.cs
@@ -0,0 +1,63 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter10_SortingAndSearching
+{
+  using System;
+
+  public class Problem_10_03_SearchInRotatedArray
+  {
+    public static int Execute(int[] input, int target)
+    {
+      if (input == null || input.Length == 0)
+      {
+        return -1;
+      }
+
+      int start = 0;
+      int end = input.Length - 1;
+      return Execute(input, start, end, target);
+    }
+
+    private static int Execute(int[] input, int start, int end, int target)
+    {
+      if (start > end)
+      {
+        return -1;
+      }
+
+      int mid = (end + start) / 2;
+      if (input[mid] == target)
+      {
+        return mid;
+      }
+
+      // Right half is ordered
+      if (input[mid] <= input[end])
+      {
+        if (target > input[mid] && target <= input[end])
+        {
+          // target is in the ordered right half
+          return Execute(input, mid + 1, end, target);
+        }
+        else
+        {
+          // target is not in the ordered right half, so it must be in the left half
+          return Execute(input, start, mid - 1, target);
+        }
+      }
+      else if (input[start] <= input[mid])
+      {
+        if (target >= input[start] && target < input[mid])
+        {
+          // left half is ordered and target is in the left half
+          return Execute(input, start, mid - 1, target);
+        }
+        else
+        {
+          // target is not in ordered left half, so it must be in right half
+          return Execute(input, mid + 1, end, target);
+        }
+      }
+
+      return -1;
+    }
+  }
+}
---
+++ Tests/Chapter10_SortingAndSearching/Problem_10_03_SearchInRotatedArrayTests.cs
@@ -0,0 +1,51 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter10_SortingAndSearching.Tests
+{
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using Solution = CrackingTheCodingInterview.Problems.Chapter10_SortingAndSearching.Problem_10_03_SearchInRotatedArray;
+
+  [TestClass]
+  public class Problem_10_03_SearchInRotatedArrayTests
+  {
+    [TestMethod]
+    public void NullTest() => Solution.Execute(null, 0).Should().Be(-1);
+
+    [TestMethod]
+    public void EmptyTest() => Solution.Execute(new int[0], 0).Should().Be(-1);
+
+    [TestMethod]
+    public void FoundAtFirstIndexTest()
+    {
+      int[] input = new int[] { 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, };
+      Solution.Execute(input, 9).Should().Be(0);
+    }
+
+    [TestMethod]
+    public void FoundAtLastIndexTest()
+    {
+      int[] input = new int[] { 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, };
+      Solution.Execute(input, 3).Should().Be(9);
+    }
+
+    [TestMethod]
+    public void NotFoundTest()
+    {
+      int[] input = new int[] { 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, };
+      Solution.Execute(input, 11).Should().Be(-1);
+    }
+
+    [TestMethod]
+    public void NotFoundDuplicatesTest()
+    {
+      int[] input = new int[] { 9, 10, 1, 2, 3, 4, 5, 5, 5, 5, };
+      Solution.Execute(input, 6).Should().Be(-1);
+    }
+
+    [TestMethod]
+    public void FoundDuplicatesTest()
+    {
+      int[] input = new int[] { 9, 10, 1, 2, 3, 4, 5, 6, 6, 8, };
+      Solution.Execute(input, 6).Should().Be(7);
+    }
+  }
+}
