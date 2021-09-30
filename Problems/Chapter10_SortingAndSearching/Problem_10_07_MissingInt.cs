eaeed027f8e1dd92169c0656a1c1cd83862edc80
Alex Vallejo <vallejo.alex@gmail.com>
2021-09-30 12:43:41 -0700

    Add problem 10.7 skeleton

---
+++ Problems/Chapter10_SortingAndSearching/Problem_10_07_MissingInt.cs
@@ -0,0 +1,15 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter10_SortingAndSearching
+{
+  using System;
+  using System.IO;
+
+  public class Problem_10_07_MissingInt
+  {
+    public static Action<string> Logger { get; set; }
+
+    public static int Execute(MemoryStream stream)
+    {
+      return -1;
+    }
+  }
+}
---
+++ Tests/Chapter10_SortingAndSearching/Problem_10_07_MissingIntTests.cs
@@ -0,0 +1,23 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter10_SortingAndSearching.Tests
+{
+  using System;
+  using System.IO;
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using Solution = CrackingTheCodingInterview.Problems.Chapter10_SortingAndSearching.Problem_10_07_MissingInt;
+
+  [TestClass]
+  public class Problem_10_07_MissingIntTests
+  {
+    [TestMethod]
+    public void DuplicateTest()
+    {
+      var stream = new MemoryStream();
+      stream.Write(BitConverter.GetBytes(1), 0 * sizeof(int), sizeof(int));
+      stream.Write(BitConverter.GetBytes(2), 1 * sizeof(int), sizeof(int));
+      stream.Write(BitConverter.GetBytes(3), 2 * sizeof(int), sizeof(int));
+      stream.Write(BitConverter.GetBytes(1), 3 * sizeof(int), sizeof(int));
+      Solution.Execute(stream).Should().Be(1);
+    }
+  }
+}
