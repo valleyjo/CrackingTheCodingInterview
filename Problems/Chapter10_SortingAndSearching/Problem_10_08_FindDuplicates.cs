e9a59d9bd6f8d0e1ea953187ab24fdec86f30214
Alex Vallejo <vallejo.alex@gmail.com>
2021-09-30 11:56:47 -0700

    Add problem 10.8

---
+++ Problems/Chapter10_SortingAndSearching/Problem_10_08_FindDuplicates.cs
@@ -0,0 +1,73 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter10_SortingAndSearching
+{
+  using System;
+  using System.Collections.Generic;
+
+  public class Problem_10_08_FindDuplicates
+  {
+    public static Action<string> Logger { get; set; }
+
+    // since this question specifies memory limits, bufffer the results in memory
+    // so the caller can do what they need to one at a time rather than storing
+    // all results in a collection
+    public static IEnumerable<int> Execute(int[] input)
+    {
+      if (input == null || input.Length == 0)
+      {
+        yield break;
+      }
+
+      var vector = new BitVector(32000);
+      foreach (int value in input)
+      {
+        if (vector.Contains(value))
+        {
+          yield return value;
+        }
+        else
+        {
+          vector.Add(value);
+        }
+      }
+    }
+
+    private static void Log(string msg) => Logger?.Invoke(msg);
+
+    // This is how the book does it too, but I don't see the need to use ints or longs to hold
+    // the bits. It seems like the simplest approach is to use a bool array.
+    // Verified with a benchmark that a bool array and a long array allocate the same memory
+    private class BitVector
+    {
+      private readonly ulong[] buckets;
+
+      public BitVector(ulong bitCount)
+      {
+        ulong bucketCount = (bitCount / sizeof(ulong)) + 1;
+        this.buckets = new ulong[bucketCount];
+      }
+
+      public bool Contains(int value) => this.Contains((ulong)value);
+
+      public bool Contains(ulong value)
+      {
+        ulong bucket = this.GetBucketContainingValue(value);
+        ulong mask = GetMask(value);
+        return (bucket & mask) != 0;
+      }
+
+      public void Add(int value) => this.Add((ulong)value);
+
+      public void Add(ulong value)
+      {
+        ulong mask = GetMask(value);
+        this.buckets[GetBucketIndexForValue(value)] |= mask;
+      }
+
+      private static ulong GetBucketIndexForValue(ulong index) => index / sizeof(ulong);
+
+      private static ulong GetMask(ulong value) => 1UL << (int)(value % sizeof(ulong));
+
+      private ulong GetBucketContainingValue(ulong value) => this.buckets[GetBucketIndexForValue(value)];
+    }
+  }
+}
---
+++ Tests/Chapter10_SortingAndSearching/Problem_10_08_FindDuplicatesTest.cs
@@ -0,0 +1,52 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter10_SortingAndSearching.Tests
+{
+  using System.Collections.Generic;
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using Microsoft.VisualStudio.TestTools.UnitTesting.Logging;
+  using Solution = CrackingTheCodingInterview.Problems.Chapter10_SortingAndSearching.Problem_10_08_FindDuplicates;
+
+  [TestClass]
+  public class Problem_10_08_FindDuplicatesTest
+  {
+    [TestInitialize]
+    public void Initialize() => Solution.Logger = (msg) => Logger.LogMessage(msg);
+
+    [TestMethod]
+    public void NullTest() => Solution.Execute(null).Should().BeEquivalentTo(new List<int>());
+
+    [TestMethod]
+    public void EmptyTest() => Solution.Execute(new int[0]).Should().BeEquivalentTo(new List<int>());
+
+    [TestMethod]
+    public void NoDuplicatesTest()
+    {
+      int[] input = new int[] { 1, 2, 3, 4, 5, 6, };
+      Solution.Execute(input).Should().BeEquivalentTo(new List<int>());
+    }
+
+    [TestMethod]
+    public void DuplicatesTest()
+    {
+      int[] input = new int[] { 1, 2, 5, 1, 5, 6, };
+      Solution.Execute(input).Should().BeEquivalentTo(new List<int>() { 1, 5 });
+    }
+
+    [TestMethod]
+    public void UseMultipleBucketsTest()
+    {
+      // fully use 6 ulong's and 10 bits from the 7th ulong
+      const int size = (5 * (1 << 6)) + 10;
+
+      // make a big array with a loop rather than inline initializing it
+      int[] input = new int[size];
+      for (int i = 1; i < size; i++)
+      {
+        // create a duplicate every 50 items
+        input[i] = i % 50 == 0 ? i - 1 : i;
+      }
+
+      Solution.Execute(input).Should().BeEquivalentTo(new List<int>() { 49, 99, 149, 199, 249, 299, });
+    }
+  }
+}
