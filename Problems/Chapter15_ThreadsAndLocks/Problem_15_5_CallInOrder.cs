c3ea85521ea07effd910cbaba7aeee0150702315
Alex Vallejo <vallejo.alex@gmail.com>
2021-10-20 08:59:13 -0700

    Add problem 15.5

---
+++ Problems/Chapter15_ThreadsAndLocks/Problem_15_5_CallInOrder.cs
@@ -0,0 +1,38 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter15_ThreadsAndLocks
+{
+  using System;
+  using System.Threading;
+  using System.Threading.Tasks;
+  using Microsoft.Extensions.Logging;
+
+  public class Problem_15_5_CallInOrder
+  {
+    private readonly SemaphoreSlim firstSemaphore = new(0, 1);
+    private readonly SemaphoreSlim secondSemaphore = new(0, 1);
+    private readonly ILogger logger;
+
+    public Problem_15_5_CallInOrder(ILogger logger) => this.logger = logger;
+
+    public void First()
+    {
+      this.logger.LogInformation(nameof(this.First) + " entering");
+      this.firstSemaphore.Release();
+      this.logger.LogInformation(nameof(this.First) + " exiting");
+    }
+
+    public async Task SecondAsync()
+    {
+      this.logger.LogInformation(nameof(this.SecondAsync) + " entering");
+      await this.firstSemaphore.WaitAsync();
+      this.secondSemaphore.Release();
+      this.logger.LogInformation(nameof(this.SecondAsync) + " exiting");
+    }
+
+    public async Task ThirdAsync()
+    {
+      this.logger.LogInformation(nameof(this.ThirdAsync) + " entering");
+      await this.secondSemaphore.WaitAsync();
+      this.logger.LogInformation(nameof(this.ThirdAsync) + " exiting");
+    }
+  }
+}
---
+++ Tests/Chapter15_ThreadsAndLocks/Problem_15_5_CallInOrderTests.cs
@@ -0,0 +1,89 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter15_ThreadsAndLocks.Tests
+{
+  using System.Collections.Generic;
+  using System.Threading.Tasks;
+  using FluentAssertions;
+  using Microsoft.Extensions.Logging;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using Solution = CrackingTheCodingInterview.Problems.Chapter15_ThreadsAndLocks.Problem_15_5_CallInOrder;
+
+  [TestClass]
+  public class Problem_15_5_CallInOrderTests
+  {
+    [TestMethod]
+    public void CallInOrderTest()
+    {
+      var logLines = new List<string>();
+      ILogger log = MemoryLog.Create(logLines);
+      var obj = new Solution(log);
+      var tasks = new List<Task>(2);
+      obj.First();
+      tasks.Add(obj.SecondAsync());
+      tasks.Add(obj.ThirdAsync());
+
+      Task.WhenAll(tasks).Wait();
+
+      logLines.Should().ContainInOrder(
+        "[Information]First exiting",
+        "[Information]SecondAsync exiting",
+        "[Information]ThirdAsync exiting");
+    }
+
+    [TestMethod]
+    public void CallBackwardsTest()
+    {
+      var logLines = new List<string>();
+      ILogger log = MemoryLog.Create(logLines);
+      var obj = new Solution(log);
+      var tasks = new List<Task>(2);
+      tasks.Add(obj.ThirdAsync());
+      tasks.Add(obj.SecondAsync());
+      obj.First();
+
+      Task.WhenAll(tasks).Wait();
+
+      logLines.Should().ContainInOrder(
+        "[Information]First exiting",
+        "[Information]SecondAsync exiting",
+        "[Information]ThirdAsync exiting");
+    }
+
+    [TestMethod]
+    public void CallSecondThirdFirstTest()
+    {
+      var logLines = new List<string>();
+      ILogger log = MemoryLog.Create(logLines);
+      var obj = new Solution(log);
+      var tasks = new List<Task>(2);
+      tasks.Add(obj.SecondAsync());
+      tasks.Add(obj.ThirdAsync());
+      obj.First();
+
+      Task.WhenAll(tasks).Wait();
+
+      logLines.Should().ContainInOrder(
+        "[Information]First exiting",
+        "[Information]SecondAsync exiting",
+        "[Information]ThirdAsync exiting");
+    }
+
+    [TestMethod]
+    public void CallSecondFirstThirdTest()
+    {
+      var logLines = new List<string>();
+      ILogger log = MemoryLog.Create(logLines);
+      var obj = new Solution(log);
+      var tasks = new List<Task>(2);
+      tasks.Add(obj.SecondAsync());
+      obj.First();
+      tasks.Add(obj.ThirdAsync());
+
+      Task.WhenAll(tasks).Wait();
+
+      logLines.Should().ContainInOrder(
+        "[Information]First exiting",
+        "[Information]SecondAsync exiting",
+        "[Information]ThirdAsync exiting");
+    }
+  }
+}
