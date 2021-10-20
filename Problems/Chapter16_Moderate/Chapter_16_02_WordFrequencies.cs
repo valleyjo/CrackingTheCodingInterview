b89186ea14ca8da05552dc240a7e5aa0f4441bc2
Alex Vallejo <vallejo.alex@gmail.com>
2021-10-20 09:00:17 -0700

    Add problem 16.1

---
+++ Problems/Chapter16_Moderate/Chapter_16_02_WordFrequencies.cs
@@ -0,0 +1,22 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter16_Moderate
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using System.Threading.Tasks;
+
+  public static class Chapter_16_02_WordFrequencies
+  {
+    public static int Execute(string word)
+    {
+      if (string.IsNullOrEmpty(word))
+      {
+        return 0;
+      }
+
+      var wordCounts = new Dictionary<string, int>();
+      return 0;
+    }
+  }
+}
