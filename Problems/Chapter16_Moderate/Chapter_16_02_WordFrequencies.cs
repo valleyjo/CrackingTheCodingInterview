ea33cf221f74cc721a6c9db58697cf39e6c267c4
Alex Vallejo <vallejo.alex@gmail.com>
2021-10-21 13:15:12 -0700

    Add 16.03

--- Problems/Chapter16_Moderate/Chapter_16_02_WordFrequencies.cs
+++ Problems/Chapter16_Moderate/Chapter_16_02_WordFrequencies.cs
@@ -2,21 +2,44 @@
 {
   using System;
   using System.Collections.Generic;
-  using System.Linq;
-  using System.Text;
-  using System.Threading.Tasks;
 
-  public static class Chapter_16_02_WordFrequencies
+  public class Chapter_16_02_WordFrequencies
   {
-    public static int Execute(string word)
+    private Dictionary<string, int> wordCounts;
+
+    public int Execute(string[] book, string wordToFind)
     {
-      if (string.IsNullOrEmpty(word))
+      if (book == null || string.IsNullOrEmpty(wordToFind))
       {
         return 0;
       }
 
-      var wordCounts = new Dictionary<string, int>();
-      return 0;
+      if (this.wordCounts == null)
+      {
+        this.wordCounts = BuildWordCounts(book);
+      }
+
+      return this.wordCounts.ContainsKey(wordToFind) ? this.wordCounts[wordToFind] : 0;
+    }
+
+    private static Dictionary<string, int> BuildWordCounts(string[] book)
+    {
+      var wordCounts = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+
+      // assume no word count more than 2^32
+      foreach (string word in book)
+      {
+        if (wordCounts.ContainsKey(word))
+        {
+          wordCounts[word]++;
+        }
+        else
+        {
+          wordCounts[word] = 1;
+        }
+      }
+
+      return wordCounts;
     }
   }
 }
