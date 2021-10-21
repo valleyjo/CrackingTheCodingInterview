8b86800d0544bacad7925b125ed11a5d790ec0aa
Alex Vallejo <vallejo.alex@gmail.com>
2021-10-21 16:11:09 -0700

    Add 16.10, 16.11 and 16.14

---
+++ Problems/Chapter16_Moderate/Chapter_16_11_DivingBoard.cs
@@ -0,0 +1,21 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter16_Moderate
+{
+  using System;
+  using System.Collections.Generic;
+
+  // TODO: Add tests
+  public static class Chapter_16_11_DivingBoard
+  {
+    public static List<int> Execute(int k, int s, int l)
+    {
+      var results = new List<int>();
+      for (int sCount = 0; sCount <= k; sCount++)
+      {
+        int lCount = k - sCount;
+        results.Add((lCount * l) + (sCount * s));
+      }
+
+      return results;
+    }
+  }
+}
---
+++ Tests/Chapter16_Moderate/Chapter_16_10_LivingPeopleTests.cs
@@ -0,0 +1,36 @@
+﻿namespace CrackingTheCodingInterview.Problems.Tests.Chapter16_Moderate
+{
+  using System;
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using Solution = CrackingTheCodingInterview.Problems.Chapter16_Moderate.Chapter_16_10_LivingPeople;
+
+  [TestClass]
+  public class Chapter_16_10_LivingPeopleTests
+  {
+    [TestMethod]
+    public void NullTest()
+    {
+      Solution.Execute(null).Should().Be(-1);
+    }
+
+    [TestMethod]
+    public void EmptyTest()
+    {
+      Solution.Execute(Array.Empty<Solution.Lifespan>()).Should().Be(-1);
+    }
+
+    [TestMethod]
+    public void BasicTest()
+    {
+      var lifespans = new Solution.Lifespan[]
+      {
+        new Solution.Lifespan(0, 90),
+        new Solution.Lifespan(5, 15),
+        new Solution.Lifespan(7, 7),
+      };
+
+      Solution.Execute(lifespans).Should().Be(7);
+    }
+  }
+}
