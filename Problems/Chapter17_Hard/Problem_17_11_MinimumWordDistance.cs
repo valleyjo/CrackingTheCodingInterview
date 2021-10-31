960429fc398314ce777912df4dfee045eb1e3103
Alex Vallejo <vallejo.alex@gmail.com>
2021-10-31 10:51:58 -0700

    Add problem 17.11

---
+++ Problems/Chapter17_Hard/Problem_17_11_MinimumWordDistance.cs
@@ -0,0 +1,77 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter17_Hard
+{
+  using System;
+  using System.Collections.Generic;
+
+  public static class Problem_17_11_MinimumWordDistance
+  {
+    private static readonly List<int> EmptyLocationList = new();
+
+    public static int Execute(string[] words, string wordOne, string wordTwo)
+    {
+      if (words == null || words.Length == 0 || string.IsNullOrEmpty(wordOne) || string.IsNullOrEmpty(wordTwo))
+      {
+        return -1;
+      }
+
+      // pre-compute
+      Dictionary<string, List<int>> wordLocations = GetWordLocations(words);
+
+      return GetSmallestDifference(GetOrEmpty(wordLocations, wordOne), GetOrEmpty(wordLocations, wordTwo));
+    }
+
+    private static List<int> GetOrEmpty(Dictionary<string, List<int>> dict, string key)
+    {
+      if (dict.TryGetValue(key, out List<int> locations))
+      {
+        return locations;
+      }
+
+      return EmptyLocationList;
+    }
+
+    private static int GetSmallestDifference(List<int> wordOneLocations, List<int> wordTwoLocations)
+    {
+      if (wordOneLocations == null || wordOneLocations.Count == 0 || wordTwoLocations == null || wordTwoLocations.Count == 0)
+      {
+        return -1;
+      }
+
+      int minDistance = int.MaxValue;
+      for (int oneIndex = 0, twoIndex = 0; oneIndex < wordOneLocations.Count && twoIndex < wordTwoLocations.Count;)
+      {
+        int oneLoc = wordOneLocations[oneIndex];
+        int twoLoc = wordTwoLocations[twoIndex];
+        minDistance = Math.Min(minDistance, Math.Abs(oneLoc - twoLoc));
+
+        if (oneLoc < twoLoc)
+        {
+          oneIndex++;
+        }
+        else
+        {
+          twoIndex++;
+        }
+      }
+
+      return minDistance;
+    }
+
+    private static Dictionary<string, List<int>> GetWordLocations(string[] words)
+    {
+      var results = new Dictionary<string, List<int>>();
+      for (int i = 0; i < words.Length; i++)
+      {
+        string word = words[i];
+        if (!results.ContainsKey(word))
+        {
+          results[word] = new List<int>();
+        }
+
+        results[word].Add(i);
+      }
+
+      return results;
+    }
+  }
+}
---
+++ Tests/Chapter17_Hard/Problem_17_11_MinimumWordDistanceTests.cs
@@ -0,0 +1,55 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter17_Hard.Tests
+{
+  using System;
+  using System.Collections.Generic;
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using Solution = CrackingTheCodingInterview.Problems.Chapter17_Hard.Problem_17_11_MinimumWordDistance;
+
+  [TestClass]
+  public class Problem_17_11_MinimumWordDistance
+  {
+    [TestMethod]
+    public void WordsParamTest()
+    {
+      Solution.Execute(null, "Fred", "Smudgie").Should().Be(-1);
+      Solution.Execute(Array.Empty<string>(), "Smudgie", "Smudgie").Should().Be(-1);
+    }
+
+    [TestMethod]
+    public void WordOneParamTest()
+    {
+      Solution.Execute(new string[] { "Smudgie", "Freddy", }, null, "Smudgie").Should().Be(-1);
+      Solution.Execute(new string[] { "Smudgie", "Freddy", }, string.Empty, "Smudgie").Should().Be(-1);
+    }
+
+    [TestMethod]
+    public void WordTwoParamTest()
+    {
+      Solution.Execute(new string[] { "Smudgie", "Freddy", }, "Smudgie", null).Should().Be(-1);
+      Solution.Execute(new string[] { "Smudgie", "Freddy", }, "Smudgie", string.Empty).Should().Be(-1);
+    }
+
+    [TestMethod]
+    public void WordOneNotExistTest()
+    {
+      string[] words = "freddy the cat loves to play with the bird toy smudgie the cat loves to be with his food freddy the cat loves treats".Split(" ");
+      Solution.Execute(words, "joey", "freddy").Should().Be(-1);
+    }
+
+    [TestMethod]
+    public void WordTwoNotExistTest()
+    {
+      string[] words = "freddy the cat loves to play with the bird toy smudgie the cat loves to be with his food freddy the cat loves treats".Split(" ");
+      Solution.Execute(words, "freddy", "joey").Should().Be(-1);
+    }
+
+    [TestMethod]
+    public void MinDistanceFoundTest()
+    {
+      string[] words = "freddy the cat loves to play with the bird toy smudgie the cat loves to lay and play freddy the cat loves treats smudgie and freddy the cat are brothers".Split(" ");
+      Solution.Execute(words, "cat", "play").Should().Be(3);
+      Solution.Execute(words, "play", "cat").Should().Be(3);
+    }
+  }
+}
