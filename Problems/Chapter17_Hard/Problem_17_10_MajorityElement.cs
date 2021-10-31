4d84b309110e2063f5a3713fcb4eaa73572c9935
Alex Vallejo <vallejo.alex@gmail.com>
2021-10-31 10:51:47 -0700

    Add problem 17.10

---
+++ Problems/Chapter17_Hard/Problem_17_10_MajorityElement.cs
@@ -0,0 +1,49 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter17_Hard
+{
+  public static class Problem_17_10_MajorityElement
+  {
+    public static int Execute(int[] input)
+    {
+      if (input == null || input.Length == 0)
+      {
+        return -1;
+      }
+
+      int value = input[0];
+      int count = 1;
+      for (int i = 1; i < input.Length; i++)
+      {
+        if (input[i] == value)
+        {
+          count++;
+        }
+        else
+        {
+          count--;
+        }
+
+        if (count == 0)
+        {
+          value = input[i];
+          count = 1;
+        }
+      }
+
+      count = 0;
+      foreach (int v in input)
+      {
+        if (v == value)
+        {
+          count++;
+        }
+      }
+
+      if (count > (input.Length / 2))
+      {
+        return value;
+      }
+
+      return -1;
+    }
+  }
+}
---
+++ Tests/Chapter17_Hard/Problem_17_10_MajorityElementTests.cs
@@ -0,0 +1,54 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter17_Hard.Tests
+{
+  using System;
+  using System.Collections.Generic;
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using Solution = CrackingTheCodingInterview.Problems.Chapter17_Hard.Problem_17_10_MajorityElement;
+
+  [TestClass]
+  public class Problem_17_10_MajorityElementTests
+  {
+    [TestMethod]
+    public void NullTest()
+    {
+      Solution.Execute(null).Should().Be(-1);
+    }
+
+    [TestMethod]
+    public void EmptyTest()
+    {
+      Solution.Execute(Array.Empty<int>()).Should().Be(-1);
+    }
+
+    [TestMethod]
+    public void OneElementTest()
+    {
+      Solution.Execute(new int[] { 0 }).Should().Be(0);
+    }
+
+    [TestMethod]
+    public void NoMajorityElementTest()
+    {
+      Solution.Execute(new int[] { 0, 1, 2, 3, 4, 5, }).Should().Be(-1);
+    }
+
+    [TestMethod]
+    public void RepeatedElementTest()
+    {
+      Solution.Execute(new int[] { 0, 0, 0, 0, 0, 0, }).Should().Be(0);
+    }
+
+    [TestMethod]
+    public void MajorityElementTest()
+    {
+      Solution.Execute(new int[] { 1, 2, 5, 9, 5, 9, 5, 5, 5, }).Should().Be(5);
+    }
+
+    [TestMethod]
+    public void OneUnderMajorityElementTest()
+    {
+      Solution.Execute(new int[] { 1, 2, 5, 9, 5, 9, 5, 5, 9, }).Should().Be(-1);
+    }
+  }
+}
