19740ce1abefd75c36b33622671f3d95714f74a0
Alex Vallejo <vallejo.alex@gmail.com>
2021-11-01 11:30:26 -0700

    Add AHeap and tests

---
+++ Problems/DataStructures/AHeap.cs
@@ -0,0 +1,136 @@
+﻿namespace CrackingTheCodingInterview.Problems.DataStructures
+{
+  using System;
+  using System.Collections.Generic;
+
+  public class AHeap
+  {
+    private readonly List<int> data;
+    private readonly int defaultValue;
+    private readonly Func<int, int, int> compare;
+
+    public AHeap(bool isMinHeap = true)
+    {
+      static int MinComparer(int x, int y)
+      {
+        if (x == y)
+        {
+          return 0;
+        }
+        else if (x < y)
+        {
+          return -1;
+        }
+        else
+        {
+          return 1;
+        }
+      }
+
+      static int MaxComparer(int x, int y)
+      {
+        if (x == y)
+        {
+          return 0;
+        }
+        else if (x < y)
+        {
+          return 1;
+        }
+        else
+        {
+          return -1;
+        }
+      }
+
+      this.compare = isMinHeap ? MinComparer : MaxComparer;
+      this.defaultValue = isMinHeap ? int.MaxValue : int.MinValue;
+      this.data = new List<int>();
+    }
+
+    public int Count => this.data.Count;
+
+    public int Peek()
+    {
+      if (this.data.Count >= 1)
+      {
+        return this.data[0];
+      }
+
+      throw new InvalidOperationException("Peek not supported when heap is empty");
+    }
+
+    public void Insert(int value)
+    {
+      this.data.Add(value);
+      this.HeapifyUp(this.data.Count - 1);
+    }
+
+    public void Remove()
+    {
+      if (this.data.Count == 0)
+      {
+        throw new InvalidOperationException("Remove not allowed when the heap is empty");
+      }
+
+      this.data[0] = this.data[^1];
+      this.data.RemoveAt(this.data.Count - 1);
+      this.HeapifyDown(0);
+    }
+
+    private void HeapifyDown(int index)
+    {
+      if (index >= this.data.Count)
+      {
+        return;
+      }
+
+      int currentValue = this.data[index];
+      int leftIndex = (index * 2) + 1;
+      int rightIndex = (index * 2) + 2;
+      int leftValue = leftIndex < this.data.Count ? this.data[leftIndex] : this.defaultValue;
+      int rightValue = rightIndex < this.data.Count ? this.data[rightIndex] : this.defaultValue;
+      int swapValue = int.MinValue;
+      int swapIndex = -1;
+
+      if (this.compare(currentValue, rightValue) <= 0 && this.compare(currentValue, leftValue) <= 0)
+      {
+        return;
+      }
+      else if (this.compare(leftValue, rightValue) < 0)
+      {
+        // swap the left child
+        swapValue = leftValue;
+        swapIndex = leftIndex;
+      }
+      else
+      {
+        // swap the right child
+        swapValue = rightValue;
+        swapIndex = rightIndex;
+      }
+
+      this.data[swapIndex] = currentValue;
+      this.data[index] = swapValue;
+      this.HeapifyDown(swapIndex);
+    }
+
+    private void HeapifyUp(int index)
+    {
+      if (index == 0)
+      {
+        return;
+      }
+
+      int currentValue = this.data[index];
+      int parentIndex = index / 2;
+      int parentValue = this.data[parentIndex];
+      if (this.compare(currentValue, parentValue) < 0)
+      {
+        this.data[parentIndex] = currentValue;
+        this.data[index] = parentValue;
+        this.HeapifyUp(parentIndex);
+      }
+    }
+  }
+}
---
+++ Tests/DataStructures/AHeapTests.cs
@@ -0,0 +1,144 @@
+﻿namespace CrackingTheCodingInterview.Tests.DataStructures
+{
+  using System;
+  using System.Collections.Generic;
+  using CrackingTheCodingInterview.Problems.DataStructures;
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+  [TestClass]
+  public class AHeapTests
+  {
+    [TestMethod]
+    public void InsertOneTest()
+    {
+      var heap = new AHeap();
+      heap.Insert(5);
+      heap.Peek().Should().Be(5);
+      heap.Count.Should().Be(1);
+    }
+
+    [TestMethod]
+    public void RemoveOneTest()
+    {
+      var heap = new AHeap();
+      heap.Insert(5);
+      heap.Remove();
+      heap.Count.Should().Be(0);
+    }
+
+    [TestMethod]
+    public void Min_InsertTest()
+    {
+      var heap = new AHeap();
+      heap.Insert(5);
+      heap.Peek().Should().Be(5);
+      heap.Count.Should().Be(1);
+
+      heap.Insert(-1);
+      heap.Peek().Should().Be(-1);
+      heap.Count.Should().Be(2);
+
+      heap.Insert(6);
+      heap.Peek().Should().Be(-1);
+      heap.Count.Should().Be(3);
+    }
+
+    [TestMethod]
+    public void Min_InsertDescendingTest()
+    {
+      var heap = new AHeap();
+      InsertAndVerify(heap, 4, 4, 1);
+      InsertAndVerify(heap, 3, 3, 2);
+      InsertAndVerify(heap, 2, 2, 3);
+      InsertAndVerify(heap, 1, 1, 4);
+      InsertAndVerify(heap, 0, 0, 5);
+    }
+
+    [TestMethod]
+    public void Min_HeapSortTest()
+    {
+      int[] values = new int[] { 8, 9, 5, 2, 4, 1, 6, 3, 0, 7, };
+      var heap = new AHeap();
+      foreach (int v in values)
+      {
+        heap.Insert(v);
+      }
+
+      for (
+        int expectedValue = 0, expectedCountBeforeRemove = values.Length;
+        expectedValue < values.Length;
+        expectedValue++, expectedCountBeforeRemove--)
+      {
+        RemoveAndVerify(heap, expectedValue, expectedCountBeforeRemove);
+      }
+
+      heap.Count.Should().Be(0);
+    }
+
+    [TestMethod]
+    public void Max_InsertTest()
+    {
+      var heap = new AHeap(false);
+      heap.Insert(5);
+      heap.Peek().Should().Be(5);
+      heap.Count.Should().Be(1);
+
+      heap.Insert(-1);
+      heap.Peek().Should().Be(5);
+      heap.Count.Should().Be(2);
+
+      heap.Insert(6);
+      heap.Peek().Should().Be(6);
+      heap.Count.Should().Be(3);
+    }
+
+    [TestMethod]
+    public void Max_InsertAscendingTest()
+    {
+      var heap = new AHeap(false);
+      InsertAndVerify(heap, 0, 0, 1);
+      InsertAndVerify(heap, 1, 1, 2);
+      InsertAndVerify(heap, 2, 2, 3);
+      InsertAndVerify(heap, 3, 3, 4);
+      InsertAndVerify(heap, 4, 4, 5);
+    }
+
+    [TestMethod]
+    public void Max_HeapSortTest()
+    {
+      int[] values = new int[] { 8, 9, 5, 2, 4, 1, 6, 3, 0, 7, };
+      var heap = new AHeap(false);
+      foreach (int v in values)
+      {
+        heap.Insert(v);
+      }
+
+      for (
+        int expectedValue = 9, expectedCountBeforeRemove = values.Length;
+        expectedValue >= 0;
+        expectedValue--, expectedCountBeforeRemove--)
+      {
+        RemoveAndVerify(heap, expectedValue, expectedCountBeforeRemove);
+      }
+
+      heap.Count.Should().Be(0);
+    }
+
+    private static void InsertAndVerify(AHeap heap, int value, int expectedPeekValueAfterInsert, int expectedCountAfterInsert)
+    {
+      heap.Count.Should().Be(expectedCountAfterInsert - 1);
+      heap.Insert(value);
+      heap.Peek().Should().Be(expectedPeekValueAfterInsert);
+      heap.Count.Should().Be(expectedCountAfterInsert);
+    }
+
+    private static void RemoveAndVerify(AHeap heap, int expectedPeekValue, int expectedCountBeforeRemove)
+    {
+      heap.Peek().Should().Be(expectedPeekValue);
+      heap.Count.Should().Be(expectedCountBeforeRemove);
+      heap.Remove();
+      heap.Count.Should().Be(expectedCountBeforeRemove - 1);
+    }
+  }
+}
