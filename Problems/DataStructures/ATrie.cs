da82cd35026dd58d8e0a21c9daa3f01f48cc8bf1
Alex Vallejo <vallejo.alex@gmail.com>
2021-10-31 22:20:50 -0700

    Add ATrie

---
+++ Problems/DataStructures/ATrie.cs
@@ -0,0 +1,92 @@
+﻿namespace CrackingTheCodingInterview.Problems.DataStructures
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using System.Threading.Tasks;
+
+  public class ATrie
+  {
+    private readonly TrieNode root = new();
+
+    public ATrie(List<string> words)
+    {
+      if (words == null || words.Count == 0)
+      {
+        return;
+      }
+
+      foreach (string word in words)
+      {
+        this.AddWord(word);
+      }
+    }
+
+    public void AddWord(string word)
+    {
+      if (!string.IsNullOrEmpty(word))
+      {
+        this.root.AddWord(word, 0);
+      }
+    }
+
+    public bool ContainsPrefix(string prefix) => this.root.ContainsPrefix(prefix, 0) != null;
+
+    public bool ContainsWord(string word)
+    {
+      TrieNode node = this.root.ContainsPrefix(word, 0);
+      return node != null && node.IsTerminal;
+    }
+
+    private class TrieNode
+    {
+      private readonly Dictionary<char, TrieNode> children = new();
+
+      public bool IsTerminal { get; private set; }
+
+      public void AddWord(string word, int index)
+      {
+        if (index == word.Length)
+        {
+          return;
+        }
+        else if (index == word.Length - 1)
+        {
+          this.IsTerminal = true;
+        }
+
+        char c = word[index];
+        if (!this.children.TryGetValue(c, out TrieNode nextNode))
+        {
+          nextNode = new TrieNode();
+          this.children[c] = nextNode;
+        }
+
+        nextNode.AddWord(word, index + 1);
+      }
+
+      public TrieNode ContainsPrefix(string prefix, int index)
+      {
+        if (string.IsNullOrEmpty(prefix) || index < 0)
+        {
+          return null;
+        }
+
+        char c = prefix[index];
+        if (this.children.ContainsKey(c))
+        {
+          // we are at the last char and it exists in the trie
+          if (prefix.Length - 1 == index)
+          {
+            return this;
+          }
+
+          return this.children[c].ContainsPrefix(prefix, index + 1);
+        }
+
+        return null;
+      }
+    }
+  }
+}
---
+++ Tests/DataStructures/ATrieTests.cs
@@ -0,0 +1,52 @@
+﻿namespace CrackingTheCodingInterview.Tests.DataStructures
+{
+  using System;
+  using System.Collections.Generic;
+  using CrackingTheCodingInterview.Problems.DataStructures;
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+  [TestClass]
+  public class ATrieTests
+  {
+    [TestMethod]
+    public void InitNullTest()
+    {
+      Action act = () => new ATrie(null);
+      act.Should().NotThrow();
+    }
+
+    [TestMethod]
+    public void InitEmptyTest()
+    {
+      Action act = () => new ATrie(new List<string>());
+      act.Should().NotThrow();
+    }
+
+    [TestMethod]
+    public void AddWordTest()
+    {
+      var trie = new ATrie(new List<string>());
+      trie.AddWord("Freddy");
+      trie.ContainsWord("Freddy").Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void ContainsPrefixTest()
+    {
+      var trie = new ATrie(new List<string>());
+      trie.AddWord("Freddy");
+      trie.ContainsPrefix("Fred").Should().BeTrue();
+      trie.ContainsPrefix("Freddy").Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void ContainsWordTest()
+    {
+      var trie = new ATrie(new List<string>());
+      trie.AddWord("Freddy");
+      trie.ContainsWord("Fred").Should().BeFalse();
+      trie.ContainsWord("Freddy").Should().BeTrue();
+    }
+  }
+}
