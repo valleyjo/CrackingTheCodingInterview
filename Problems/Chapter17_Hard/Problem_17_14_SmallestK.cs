d36121aa6921b35f0e634d1c439957396c1dd9e9
Alex Vallejo <vallejo.alex@gmail.com>
2021-11-02 08:45:16 -0700

    Add problem 71.14

---
+++ Problems/Chapter17_Hard/Problem_17_14_SmallestK.cs
@@ -0,0 +1,40 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter17_Hard
+{
+  using System;
+  using System.Collections.Generic;
+  using CrackingTheCodingInterview.Problems.DataStructures;
+
+  public static class Problem_17_14_SmallestK
+  {
+    public static List<int> Execute(int[] input, int k)
+    {
+      var results = new List<int>();
+      if (input == null || input.Length == 0 || k < 1)
+      {
+        return results;
+      }
+
+      var maxHeap = new AHeap(isMinHeap: false);
+      foreach (int v in input)
+      {
+        if (maxHeap.Count < k)
+        {
+          maxHeap.Insert(v);
+        }
+        else if (v < maxHeap.Peek())
+        {
+          maxHeap.Remove();
+          maxHeap.Insert(v);
+        }
+      }
+
+      while (maxHeap.Count > 0)
+      {
+        results.Add(maxHeap.Peek());
+        maxHeap.Remove();
+      }
+
+      return results;
+    }
+  }
+}
---
+++ Tests/Chapter17_Hard/Problem_17_14_SmallestKTests.cs
@@ -0,0 +1,54 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter17_Hard.Tests
+{
+  using System;
+  using System.Collections.Generic;
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using Solution = CrackingTheCodingInterview.Problems.Chapter17_Hard.Problem_17_14_SmallestK;
+
+  [TestClass]
+  public class Problem_17_14_SmallestKTests
+  {
+    [TestMethod]
+    public void NullTest()
+    {
+      Solution.Execute(null, 1).Should().BeEquivalentTo(new List<int>());
+    }
+
+    [TestMethod]
+    public void EmptyTest()
+    {
+      Solution.Execute(Array.Empty<int>(), 1).Should().BeEquivalentTo(new List<int>());
+    }
+
+    [TestMethod]
+    public void BadKTest()
+    {
+      Solution.Execute(new int[] { 1, 2, 3, 4, }, -5).Should().BeEquivalentTo(new List<int>());
+    }
+
+    [TestMethod]
+    public void SortedAscendingTest()
+    {
+      Solution.Execute(new int[] { 1, 2, 3, 4, }, 2).Should().BeEquivalentTo(new List<int>() { 1, 2, });
+    }
+
+    [TestMethod]
+    public void SortedDescendingTest()
+    {
+      Solution.Execute(new int[] { 5, 4, 3, 2, }, 3).Should().BeEquivalentTo(new List<int>() { 2, 3, 4, });
+    }
+
+    [TestMethod]
+    public void NotSortedTest()
+    {
+      Solution.Execute(new int[] { 50, 12, 3, 9, 15, -40, 17 }, 4).Should().BeEquivalentTo(new List<int>() { -40, 3, 9, 12, });
+    }
+
+    [TestMethod]
+    public void KSmallerThanLength()
+    {
+      Solution.Execute(new int[] { 50, 12, 3, 9, 15, -40, 17 }, 8).Should().BeEquivalentTo(new List<int>() { 50, 12, 3, 9, 15, -40, 17 });
+    }
+  }
+}
