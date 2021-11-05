d98500fc5053c60da10c22346b65625a23364a8c
Alex Vallejo <vallejo.alex@gmail.com>
2021-11-05 11:25:57 -0700

    Add problem 17.16

---
+++ Problems/Chapter17_Hard/Problem_17_16_Masseuse.cs
@@ -0,0 +1,60 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter17_Hard
+{
+  using System;
+  using System.Collections.Generic;
+  using CrackingTheCodingInterview.Problems.DataStructures;
+
+  public static class Problem_17_16_Masseuse
+  {
+    /*
+    requirements:
+    return number of minutes to maximize
+
+    assumptions:
+    valid input -> not empty not null
+    input.Length >= 2
+
+    signiture:
+    int FindMaxMinutes(int[] minutes)
+
+    Some test cases:
+    equal amounts
+    multiple paths to the same max amount
+
+    Approaches:
+    recursive + memoization 2^n
+    bottom-up N time N space
+    bottom-up two variables? N time 1 space
+
+    Steps:
+    start at back of array - 2
+    init two max values to end and end -1
+    take max of current + skip OR next
+     */
+    public static int Execute(int[] input)
+    {
+      if (input == null || input.Length == 0)
+      {
+        return -1;
+      }
+
+      if (input.Length == 1)
+      {
+        return input[0];
+      }
+
+      int current = Math.Max(input[^1], input[^2]);
+      int skipOne = current;
+      int next = current;
+
+      for (int i = input.Length - 3; i >= 0; i--)
+      {
+        current = Math.Max(input[i] + skipOne, next);
+        skipOne = next;
+        next = current;
+      }
+
+      return current;
+    }
+  }
+}
---
+++ Tests/Chapter17_Hard/Problem_17_16_MasseuseTests.cs
@@ -0,0 +1,47 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter17_Hard.Tests
+{
+  using System;
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using Solution = CrackingTheCodingInterview.Problems.Chapter17_Hard.Problem_17_16_Masseuse;
+
+  [TestClass]
+  public class Problem_17_16_MasseuseTests
+  {
+    [TestMethod]
+    public void NullTest()
+    {
+      Solution.Execute(null).Should().Be(-1);
+    }
+
+    [TestMethod]
+    public void EmptyTest()
+    {
+      Solution.Execute(Array.Empty<int>()).Should().Be(-1);
+    }
+
+    [TestMethod]
+    public void BiggestAtEndTest()
+    {
+      Solution.Execute(new int[] { 1, 2, 4, 2, 5, 60, }).Should().Be(65);
+    }
+
+    [TestMethod]
+    public void EqualValuesTest()
+    {
+      Solution.Execute(new int[] { 30, 15, 60, 75, 45, 15, 15, 45 }).Should().Be(180);
+    }
+
+    [TestMethod]
+    public void OneElementTest()
+    {
+      Solution.Execute(new int[] { 30, }).Should().Be(30);
+    }
+
+    [TestMethod]
+    public void TwoElementTest()
+    {
+      Solution.Execute(new int[] { 30, 15, }).Should().Be(30);
+    }
+  }
+}
