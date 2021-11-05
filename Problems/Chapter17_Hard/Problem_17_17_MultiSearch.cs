0fa37d5d947611a87449995adca3c487110dfb5d
Alex Vallejo <vallejo.alex@gmail.com>
2021-11-05 11:26:15 -0700

    Add problem 17.17

---
+++ Problems/Chapter17_Hard/Problem_17_17_MultiSearch.cs
@@ -0,0 +1,79 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter17_Hard
+{
+  using System;
+  using System.Collections.Generic;
+  using CrackingTheCodingInterview.Problems.DataStructures;
+
+  public static class Problem_17_17_MultiSearch
+  {
+    /*
+    requirements:
+    return all matches for all T
+
+    assumptions:
+    valid input -> not empty not null
+    input.Length >= 2
+
+    signiture:
+    List<Match> FindMatches(string input, string[] targets)
+    Match:
+    - int index
+    - string value (a value from target)
+
+    Some test cases:
+
+    Approaches:
+    brute force -> foreach T for each index in b see if it matches -> time: B*T space: 1
+    foreach char in B -> foreach sub-word until longest word in T -> see if in Trie, if yes, add match
+
+    Steps:
+
+    */
+    public static List<Match> Execute(string input, string[] targets)
+    {
+      var results = new List<Match>();
+      if (string.IsNullOrEmpty(input) || targets == null || targets.Length == 0)
+      {
+        return results;
+      }
+
+      var trie = new ATrie(targets);
+      for (int start = 0; start < input.Length; start++)
+      {
+        for (int end = start + 1; end <= input.Length; end++)
+        {
+          string substring = input.Substring(start, end - start);
+
+          // no word in targets starts with this prefix, no need to continue with this prefix
+          if (!trie.ContainsPrefix(substring))
+          {
+            break;
+          }
+
+          if (trie.ContainsWord(substring))
+          {
+            results.Add(new Match(start, end, substring));
+          }
+        }
+      }
+
+      return results;
+    }
+
+    public readonly struct Match
+    {
+      public Match(int start, int end, string value)
+      {
+        this.Start = start;
+        this.End = end;
+        this.Value = value;
+      }
+
+      public string Value { get; }
+
+      public int Start { get; }
+
+      public int End { get; }
+    }
+  }
+}
---
+++ Tests/Chapter17_Hard/Problem_17_17_MultiSearchTests.cs
@@ -0,0 +1,49 @@
+﻿namespace CrackingTheCodingInterview.Problems.Chapter17_Hard.Tests
+{
+  using System;
+  using System.Collections.Generic;
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using Solution = CrackingTheCodingInterview.Problems.Chapter17_Hard.Problem_17_17_MultiSearch;
+
+  [TestClass]
+  public class Problem_17_17_MultiSearchTests
+  {
+    [TestMethod]
+    public void NullTest()
+    {
+      Solution.Execute(null, null).Should().BeEquivalentTo(new List<Solution.Match>());
+    }
+
+    [TestMethod]
+    public void EmptyTest()
+    {
+      Solution.Execute(string.Empty, Array.Empty<string>()).Should().BeEquivalentTo(new List<Solution.Match>());
+    }
+
+    [TestMethod]
+    public void NoMatchesTest()
+    {
+      Solution.Execute("freddy", new string[] { "z", "ba", "del", });
+    }
+
+    [TestMethod]
+    public void MultipleMatchesTest()
+    {
+      var expected = new List<Solution.Match>()
+      {
+        new Solution.Match(0, 4, "miss"),
+        new Solution.Match(1, 4, "iss"),
+        new Solution.Match(4, 6, "iss"),
+        new Solution.Match(9, 10, "pp"),
+        new Solution.Match(1, 1, "i"),
+        new Solution.Match(1, 1, "i"),
+        new Solution.Match(5, 5, "i"),
+        new Solution.Match(7, 7, "i"),
+        new Solution.Match(10, 10, "i"),
+      };
+
+      Solution.Execute("mississippi", new string[] { "miss", "iss", "pp", "i", });
+    }
+  }
+}
